a3db3c30bc5a18219b61ffa3d1ba2f7972352fe6
Paul Cowan <paul.cowan@bjss.com>
2017-11-25 14:51:48 +0000

    bye bye next, hello express

--- components/App/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var withStyles_1 = require("isomorphic-style-loader/lib/withStyles");
-var styles = require('./App.scss');
-var AppView = function () { return (React.createElement("div", { className: styles.container },
-    React.createElement("h1", null, "This is the app"))); };
-exports.App = withStyles_1.default(styles)(AppView);
-//# sourceMappingURL=App.js.map
--- pages/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var App_1 = require("../components/App/App");
-var pageWithStyles_1 = require("../components/pageWithStyles/pageWithStyles");
-console.log(pageWithStyles_1.pageWithStyles);
-var Index = function () { return React.createElement(App_1.App, null); };
-exports.default = pageWithStyles_1.pageWithStyles(Index);
-//# sourceMappingURL=index.js.map
