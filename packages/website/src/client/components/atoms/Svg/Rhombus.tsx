a74f1d6c8c955273522eda7e80f33c8d68ac8bde
Paul Cowan <paul.cowan@bjss.com>
2018-01-07 19:25:49 +0000

    use tsconfig.base.json

---
+++ packages/website/src/client/components/atoms/Svg/Rhombus.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface RhombusProps {
+  className?: string;
+}
+
+export const Rhombus: React.StatelessComponent<RhombusProps> = ({ className }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" className={className} viewBox="0 0 40 40">
+    <path d="M20 0 L40 20 L20 40 L0 20" />
+  </svg>
+);
---
+++ packages/website/src/client/components/organisms/Nav/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export interface NavProps {}
+
+export const Nav: React.StatelessComponent<NavProps> = ({ children }) => <Nav>{children}</Nav>;
