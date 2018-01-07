b79125f711feda4d11f69fbc66eb6de0f9303d59
Paul Cowan <paul.cowan@bjss.com>
2018-01-07 11:16:50 +0000

    move start into grunt

--- packages/website/Gruntfile.js
+++ packages/website/Gruntfile.js
@@ -2,6 +2,7 @@
 
 const webpack = require('webpack');
 const path = require('path');
+const { merge } = require('lodash');
 
 module.exports = grunt => {
   require('load-grunt-tasks')(grunt, {
@@ -11,18 +12,56 @@ module.exports = grunt => {
   });
 
   grunt.initConfig({
+    env: {
+      dev: {
+        NODE_ENV: 'development'
+      },
+      prod: {
+        NODE_ENV: 'production'
+      }
+    },
+    open: {
+      dev: {
+        path: 'http://localhost:3000'
+      }
+    },
+    nodemon: {
+      dev: {
+        script: './src/index.tsx',
+        options: {
+          ext: 'ts,tsx',
+          wtach: ['src/server/**/*', 'src/shared/**/*'],
+          cwd: __dirname,
+          exec: '../../node_modules/.bin/ts-node'
+        }
+      }
+    },
     ts: {
-      options: require('./tsconfig.json').compilerOptions,
-      default: ['src/client/**/*.ts', 'src/client/**/*.tsx', '!node_modules/**'],
-      outDir: 'build'
+      node: {
+        src: ['src/server/**/*.ts', 'src/server/**/*.tsx', '!node_modules/**'],
+        options: {
+          fast: 'never'
+        }
+      }
     },
     clean: 'build',
-    webpack: {
-      dev: require('../../webpack/client'),
-      server: require('../../webpack/server')
-    },
-    start: {}
+    watch: {
+      node: {
+        files: ['./src/server/**/*.ts', './src/server/**/*.tsx'],
+        options: { spawn: false },
+        tasks: ['ts:node']
+      }
+    }
+    /*     
+    , webpack: {
+      dev: require('../../webpack/client').configure({
+        entryPoint: path.join(process.cwd(), 'src/client/index')
+      }),
+      server: require('../../webpack/server').configure()
+    }, */
   });
 
   grunt.registerTask('build', ['clean', 'webpack:dev']);
+  grunt.registerTask('server', ['env:dev', 'nodemon:dev', 'open:dev']);
+  grunt.registerTask('start', ['server']);
 };
