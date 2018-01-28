28007b280278d92286736b8cb1aa6dba2de635dc
Paul Cowan <paul.cowan@bjss.com>
2018-01-28 14:51:00 +0000

    more menu items

---
+++ packages/website/src/shared/components/Blog/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { pageMaker } from '../PageMaker';
+
+export const Blog: React.StatelessComponent = () => <h1>Blog</h1>;
+
+export default pageMaker({ Comp: Blog });
