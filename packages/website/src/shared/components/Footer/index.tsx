ae74ada8719bdd7ba3eff3665b396968d7362e61
Paul Cowan <paul.cowan@bjss.com>
2018-02-04 15:58:25 +0000

    responsive footer

--- packages/website/src/shared/components/Footer/index.tsx
+++ packages/website/src/shared/components/Footer/index.tsx
@@ -8,43 +8,53 @@ export const Footer: React.StatelessComponent = () => (
   <footer className="wrapper">
     <Wrap className="wrapper">
       <Layout>
-        <GelItem s="1/5">
-          <div className={styles.logo__container}>
-            <div>
-              <Cow />
-            </div>
-          </div>
-          <span className={styles.name}>Paul Cowan</span>
+        <GelItem m="1/3">
+          <Layout>
+            <GelItem m="1/2" className={styles.logo}>
+              <div className={styles.logo__container}>
+                <div>
+                  <Cow />
+                </div>
+              </div>
+              <span className={styles.name}>Paul Cowan</span>
+            </GelItem>
+            <GelItem m="1/2">
+              <ul className={styles.links}>
+                <li>
+                  <a href="#">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="#">Terms of Service</a>
+                </li>
+              </ul>
+            </GelItem>
+          </Layout>
         </GelItem>
-        <GelItem s="1/5">
-          <ul className={styles.links}>
-            <li>
-              <a href="#">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="#">Terms of Service</a>
-            </li>
-          </ul>
-        </GelItem>
-        <GelItem s="3/5" className={styles.contact}>
-          <div>
-            <ul className={styles.social}>
-              <li>
-                <a href="https://github.com/dagda1" target="_blank">
-                  <Github />
+        <GelItem m="2/3">
+          <Layout>
+            <GelItem className={styles.contact}>
+              <div>
+                <ul className={styles.social}>
+                  <li>
+                    <a href="https://github.com/dagda1" target="_blank">
+                      <Github />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://twitter.com/dagda1" target="_blank">
+                      <Twitter />
+                    </a>
+                  </li>
+                </ul>
+                <a className={styles.email} href="mailto:dagda1@scotalt.net">
+                  dagda1@scotalt.net
                 </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/dagda1" target="_blank">
-                  <Twitter />
-                </a>
-              </li>
-            </ul>
-            <a className={styles.email} href="mailto:dagda1@scotalt.net">
-              dagda1@scotalt.net
-            </a>
-          </div>
-          <div className={styles.copyright}>Copyright © Cutting-Edge Solutions (Scotland) inc. Al rights reserved</div>
+              </div>
+              <div className={styles.copyright}>
+                Copyright © Cutting-Edge Solutions (Scotland) inc. Al rights reserved
+              </div>
+            </GelItem>
+          </Layout>
         </GelItem>
       </Layout>
     </Wrap>
