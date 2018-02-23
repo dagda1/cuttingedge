7aba4b2f05912aa933ea37fa762ae52e4ecae8fb
Paul Cowan <paul.cowan@bjss.com>
2018-02-23 07:05:34 +0000

    dockerise it

---
+++ packages/website/nginx/bin/develop.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+#start development server on :8000
+
+source ./bin/env.sh
+
+dcdev build
+dcdev up
---
+++ packages/website/nginx/bin/env.sh
@@ -0,0 +1,11 @@
+export DOCKER_CONFIG_PROD=${DOCKER_CONFIG_PROD:-docker-compose.production.yml}
+export DOCKER_CONFIG_DEV=${DOCKER_CONFIG_DEV:-docker-compose.development.yml}
+
+
+dcdev() {
+    NODE_ENV=production docker-compose -f $DOCKER_CONFIG_DEV "$@"
+}
+
+dcprod() {
+    NODE_ENV=production docker-compose -f $DOCKER_CONFIG_PROD "$@"
+}
