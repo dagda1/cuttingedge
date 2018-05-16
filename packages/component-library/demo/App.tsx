8f452af52f44e8771703f566a1a537a5fc283dd8
Paul Cowan <paul.cowan@bjss.com>
2018-05-16 20:22:27 +0100

    remove mixins from react-gel

--- packages/component-library/demo/App.tsx
+++ packages/component-library/demo/App.tsx
@@ -1,6 +1,6 @@
 import 'font-awesome/css/font-awesome.css';
 import * as React from 'react';
-import { Rhombus, Cow, Modal } from '../src/components';
+import { Rhombus, Cow, Modal, Heading, FormInput, FormSelect } from '../src/components';
 import { Wrap, Layout, GelItem } from '@cutting/react-gel';
 import { Button, ButtonStyle } from '../src/components/atoms/Button';
 
@@ -25,6 +25,72 @@ export class App extends React.Component<AppProps, AppState> {
   render() {
     return (
       <Wrap>
+        <Layout>
+          <GelItem>
+            <Heading level={2}>Inputs</Heading>
+          </GelItem>
+        </Layout>
+        <Layout>
+          <GelItem l="1/4">
+            <FormInput
+              highlight
+              label="Highlight"
+              maxLength={100}
+              onKeyDown={e => {
+                console.log(e);
+                return true;
+              }}
+            />
+          </GelItem>
+          <GelItem l="1/4">
+            <FormInput label="Invalid" invalid errorMessage="Error Message" />
+          </GelItem>
+          <GelItem l="1/4">
+            <FormInput label="Invalid & required" invalid required errorMessage="Error Message" />
+          </GelItem>
+          <GelItem l="1/4">
+            <FormInput
+              strong={false}
+              label="This is a really, really, really, really, really, really, really, really, really, really long bit of text"
+            />
+          </GelItem>
+        </Layout>
+        <Layout>
+          <GelItem>
+            <Heading level={2}>Selects</Heading>
+          </GelItem>
+        </Layout>
+        <Layout>
+          <GelItem s="1/4">
+            <FormSelect
+              label="Select"
+              options={[
+                { key: 'Mr', value: 'Mr' },
+                { key: 'Mrs', value: 'Mrs' },
+                { key: 'Miss', value: 'Miss' },
+                { key: 'Ms', value: 'Ms' },
+                { key: 'Dr', value: 'Dr' },
+                { key: 'Reverend', value: 'Reverend' },
+                { key: 'Professor', value: 'Professor' },
+                { key: 'Other', value: 'Other' }
+              ]}
+              optionKey="key"
+              valueKey="value"
+              dividerAt={1}
+            />
+          </GelItem>
+          <GelItem s="1/4">
+            <FormSelect
+              label="invalid"
+              invalid
+              defaultLabel="Select a country"
+              options={[{ id: 'uk', displayName: 'United Kingdom' }]}
+              valueKey="id"
+              optionKey="displayName"
+              errorMessage="Invalid Select"
+            />
+          </GelItem>
+        </Layout>
         <Layout>
           <GelItem>
             <Modal
