4f1e28164b726b47298b8a2d983298295c97045f
Paul Cowan <paul.cowan@bjss.com>
2019-03-18 18:35:03 +0000

    ts-build find tsc and tslint

--- packages/devtools/scripts/ts-build.js
+++ packages/devtools/scripts/ts-build.js
@@ -6,6 +6,22 @@ const copy = require('copy');
 const chalk = require('chalk');
 const { exec } = require('child_process');
 
+const MaxTries = 15;
+
+function findExecutable(current, executable, tries = 0) {
+  const modulesDir = path.resolve(current, 'node_modules', '.bin', executable);
+
+  if (tries === MaxTries) {
+    throw new Error(`cannot find ${tsc} in ${modulesDir}`);
+  }
+
+  if (fs.existsSync(modulesDir)) {
+    return modulesDir;
+  }
+
+  return findExecutable(path.resolve(current, '..'), executable, tries++);
+}
+
 function runTypeScriptBuild() {
   const buildConfig = require(paths.jsBuildConfigPath).ts;
   const {
@@ -22,13 +38,13 @@ function runTypeScriptBuild() {
   process.argv.push('--pretty', true);
   process.argv.push('--sourceMap', process.argv.includes('--source-map'));
 
-  const tscPath = path.join(__dirname, '../node_modules/.bin/tsc');
+  const tscPath = findExecutable(__dirname, 'tsc');
   const tsc = exec(`${tscPath} ${process.argv.slice(2).join(' ')}`);
 
-  tsc.stdout.on('data', (data) => console.log(chalk.red(data)));
-  tsc.stderr.on('data', (data) => console.error(chalk.red(data)));
+  tsc.stdout.on('data', data => console.log(chalk.red(data)));
+  tsc.stderr.on('data', data => console.error(chalk.red(data)));
 
-  tsc.on('close', (code) => {
+  tsc.on('close', code => {
     console.log(chalk.cyan(`tsc exited with code ${code}`));
 
     if (code !== 0) {
@@ -42,13 +58,13 @@ function runTypeScriptBuild() {
 function runTsLint() {
   console.log(`Running tslint`);
 
-  const tslintPath = path.join(__dirname, '../node_modules/.bin/tslint');
+  const tslintPath = findExecutable(__dirname, 'tslint');
   const tslint = exec(`${tslintPath} -c ${paths.tsLintConfig} -p ${paths.tsConfig} -t stylish`);
 
-  tslint.stdout.on('data', (data) => console.log(chalk.red(data)));
-  tslint.stderr.on('data', (data) => console.error(chalk.red(data)));
+  tslint.stdout.on('data', data => console.log(chalk.red(data)));
+  tslint.stderr.on('data', data => console.error(chalk.red(data)));
 
-  tslint.on('close', (code) => {
+  tslint.on('close', code => {
     console.log(chalk.cyan(`tslint exited with code ${code}`));
 
     if (code !== 0) {
@@ -61,7 +77,7 @@ function build() {
     runTypeScriptBuild();
 
     const patterns = ['*.scss', '*.css', '*.png', '*.jpg', '*.md', '*.svg'].map(
-      (pattern) => `${paths.appSrc}/**/${pattern}`
+      pattern => `${paths.appSrc}/**/${pattern}`
     );
 
     copy(patterns, paths.appBuild, (err, files) => {
