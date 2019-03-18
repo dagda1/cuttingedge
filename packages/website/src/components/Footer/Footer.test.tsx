4e689b5c149402f048d09fe5c060febc0642e25d
Paul Cowan <paul.cowan@bjss.com>
2019-03-18 22:21:14 +0000

    fix website tests

--- packages/website/src/components/Footer/Footer.test.tsx
+++ packages/website/src/components/Footer/Footer.test.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { Footer } from '.';
 import { wrapComponentInReduxForTesting } from '../../tests';
 import { NavLink } from 'react-router-dom';
 
 describe('<Footer />', () => {
   it('should render footer', () => {
-    const footer = wrapComponentInReduxForTesting(Footer);
+    const footer = wrapComponentInReduxForTesting(Footer, {}, {});
 
     expect(footer.find(NavLink).length).toBeGreaterThan(0);
   });
