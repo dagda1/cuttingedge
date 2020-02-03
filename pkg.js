8908ce2c12194c277719727578ae1c5279628aca
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-02-03 22:27:36 +0000

    unify version

--- pkg.js
+++ pkg.js
@@ -1,5 +1,5 @@
 const path = require('path');
 
-module.exports = ['util', 'component-library', 'connected-components', 'website'].map((pkg) =>
+module.exports = ['util', 'component-library', 'connected-components', 'hooks', 'website'].map((pkg) =>
   path.resolve(process.cwd(), `./packages/${pkg}`)
 );
