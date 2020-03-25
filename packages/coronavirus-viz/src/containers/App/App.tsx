2166ca70290ad1fad27a3d656aad6f5095ffb0c3
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-03-25 08:07:50 +0000

    coronavirus initial styles

--- packages/coronavirus-viz/src/containers/App/App.tsx
+++ packages/coronavirus-viz/src/containers/App/App.tsx
@@ -2,7 +2,7 @@
 // eslint:disable
 import React from 'react';
 
-// const styles = require('./global.module.scss');
+const styles = require('../../styles/global.module.scss');
 
 export const App: React.FC = () => {
   return <h1>Foo</h1>;
