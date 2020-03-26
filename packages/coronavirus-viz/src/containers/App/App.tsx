bb19ed88b37c0fb523e84809b731297852640885
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-03-26 09:37:33 +0000

    display some data

--- packages/coronavirus-viz/src/containers/App/App.tsx
+++ packages/coronavirus-viz/src/containers/App/App.tsx
@@ -21,18 +21,32 @@ type Results = {
   result: Result;
 };
 
-type DayData = Result & { date: string; country: Countries };
+type DayData = Result & {
+  date: string;
+  country: Countries;
+  delta: number;
+};
 
 // documenter.getpostman.com/view/2568274/SzS8rjbe?version=latest
 const baseUrl = 'https://covidapi.info/api/v1/country';
 
 const transform = (results: Results, country: Countries): DayData[] => {
-  const data: DayData[] = Object.keys(results.result).map(k => ({
-    date: dayjs(k).format('DD/MM/YYYY'),
-    ...results.result[k],
-  }));
+  const data: DayData[] = Object.keys(results.result)
+    .map(k => ({
+      country,
+      date: dayjs(k).format('DD/MM/YYYY'),
+      ...results.result[k],
+    }))
+    .filter(d => d.deaths > 0);
 
-  return data;
+  const result = data.map((d, i) => {
+    return {
+      ...d,
+      delta: i === 0 ? 0 : d.deaths - data[i - 1].deaths,
+    };
+  });
+
+  return result;
 };
 
 const getCountriesData = () => {
@@ -43,14 +57,16 @@ const getCountriesData = () => {
     fetch(`${baseUrl}/${Countries.IT}`, { headers }),
     fetch(`${baseUrl}/${Countries.USA}`, { headers }),
     fetch(`${baseUrl}/${Countries.China}`, { headers }),
-  ]).then(async result => {
-    const gb = transform(await result[0].json(), Countries.GB);
-    const italy = transform(await result[1].json(), Countries.IT);
-    const usa = transform(await result[2].json(), Countries.USA);
-    const china = transform(await result[3].json(), Countries.China);
+  ])
+    .then(async result => {
+      const gb = transform(await result[0].json(), Countries.GB);
+      const italy = transform(await result[1].json(), Countries.IT);
+      const usa = transform(await result[2].json(), Countries.USA);
+      const china = transform(await result[3].json(), Countries.China);
 
-    return { gb, italy, usa, china };
-  });
+      return { gb, italy, usa, china };
+    })
+    .catch(console.error);
 };
 
 export const App: React.FC = () => {
@@ -64,7 +80,34 @@ export const App: React.FC = () => {
     return <div>{error}</div>;
   }
 
-  console.log(data);
-
-  return <h1>Foo</h1>;
+  return (
+    <div>
+      <table style={{ width: '100%' }}>
+        <tbody>
+          {Object.keys(data).map((k, i) => {
+            const country = data[k];
+            return (
+              <React.Fragment key={i}>
+                <tr>
+                  <td>{k}</td>
+                </tr>
+                <tr>
+                  {country.map((c: DayData, i: number) => (
+                    <React.Fragment key={i}>
+                      <td style={{ textAlign: 'right', whiteSpace: 'nowrap' }}>
+                        {c.date}
+                        <br /> confirmed = {c.confirmed}
+                        <br /> deaths = {c.deaths}
+                        <br /> delta = {c.delta}
+                      </td>
+                    </React.Fragment>
+                  ))}
+                </tr>
+              </React.Fragment>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
