7b2e4a85e9cd81ee663635b1195730b683c0f645
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-03-28 15:16:28 +0000

    bump version to 2.3.2

--- packages/website/bin/deploy.sh
+++ packages/website/bin/deploy.sh
@@ -9,7 +9,7 @@ dcprod build
 ./bin/stop_production.sh
 ./bin/start_production.sh
 
-$(aws ecr get-login --no-include-email --region us-east-1)
+$(aws ecr get-login-password --no-include-email --region us-east-1)
 
 docker tag website_nginx:latest 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_nginx:latest
 docker tag website_server:latest 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_server:latest
