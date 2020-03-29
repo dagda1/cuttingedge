796dafcb0f36d19aa148a893c1215aae7fdd0cbc
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-03-29 09:08:48 +0100

    shorter title

--- packages/website/src/components/Graph/index.tsx
+++ packages/website/src/components/Graph/index.tsx
@@ -125,7 +125,7 @@ export const Graph: React.FC = () => {
 
   return (
     <ApplicationLayout
-      heading={`Covid-19 deaths over days since first death on ${dayjs()
+      heading={`Covid-19 deaths over days since first death.  Data gathered on ${dayjs()
         .subtract(1, 'day')
         .format('DD/MM/YYYY')}`}
     >
