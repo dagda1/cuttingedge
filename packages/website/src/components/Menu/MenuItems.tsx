8dd2f5ebef8ada7295da1cf36ca446a6d2de4b1c
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-04-28 21:27:04 +0100

    mobile menu

--- packages/website/src/components/Menu/MenuItems.tsx
+++ packages/website/src/components/Menu/MenuItems.tsx
@@ -28,7 +28,14 @@ export const MenuItems: React.FC<MenuItemsProps> = ({ collapse }) => (
 
 export const MobileMenuItems: React.FC<MenuItemsProps> = ({ collapse }) => {
   return (
-    <>
+    <div>
+      <li
+        className={cs(styles.horizontal, styles.mobile, styles.close__button)}
+      >
+        <button type="button" onClick={collapse}>
+          X
+        </button>
+      </li>
       <MenuItems collapse={collapse} />
       {[
         {
@@ -56,6 +63,6 @@ export const MobileMenuItems: React.FC<MenuItemsProps> = ({ collapse }) => {
           </li>
         );
       })}
-    </>
+    </div>
   );
 };
