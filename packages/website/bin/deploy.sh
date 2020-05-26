4ab034d3b6f123dd7b00b8e97bebb9928778ca03
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-05-26 17:57:38 +0100

    fix deploy

--- packages/website/bin/deploy.sh
+++ packages/website/bin/deploy.sh
@@ -4,12 +4,16 @@ source ./bin/env.sh
 
 echo "building backend"
 
+cp public/favicon.ico dist/public
+
 dcprod build
+
 
 ./bin/stop_production.sh
 ./bin/start_production.sh
 
 $(aws ecr get-login --no-include-email --region us-east-1)
+
 
 docker tag website_nginx:latest 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_nginx:latest
 docker tag website_server:latest 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_server:latest
