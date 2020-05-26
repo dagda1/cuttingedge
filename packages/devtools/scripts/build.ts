03a650ffa52d47fb04133dc4a04054c2f9dee492
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-05-26 20:51:55 +0100

    build.js => build.ts

---
+++ packages/devtools/scripts/build.ts
@@ -0,0 +1,78 @@
+#! /usr/bin/env node
+'use strict';
+process.env.NODE_ENV = 'production';
+
+process.on('unhandledRejection', (err) => {
+  throw err;
+});
+
+import '.../config/env';
+
+import fs from 'fs-extra';
+import paths from '../config/paths';
+import logger from './logger';
+import FileSizeReporter from 'react-dev-utils/FileSizeReporter';
+import { copyPublicFolder } from './utils/copy-public-folder';
+import { compile } from './webpack/compile';
+
+const measureFileSizesBeforeBuild = FileSizeReporter.measureFileSizesBeforeBuild;
+const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
+const { copyPublicFolder } = require('./utils/copy-public-folder');
+const { compile } = require('./webpack/compile');
+const merge = require('webpack-merge');
+
+const configureWebpackClient = require('../webpack/client').configure;
+const configureWebpackServer = require('../webpack/server').configure;
+const configureWebpackNode = require('../webpack/node').configure;
+
+export const build = async ({
+  buildClient,
+  buildServer,
+  buildNode,
+}: {
+  buildClient: boolean;
+  buildServer: boolean;
+  buildNode: boolean;
+}) => {
+  logger.start('starting build');
+  const globalBuildConfig = require(paths.jsBuildConfigPath);
+
+  const localBuildConfig = fs.existsSync(paths.localBuildConfig) ? require(paths.localBuildConfig) : {};
+
+  const buildConfig = merge(globalBuildConfig, localBuildConfig);
+
+  const clientConfig = !!buildClient && configureWebpackClient(buildConfig.client);
+  const serverConfig = !!buildServer && configureWebpackServer(buildConfig.server);
+  const nodeConfig = !!buildNode && configureWebpackNode(buildConfig.node);
+
+  const publicDir = buildServer ? paths.appBuildPublic : paths.appBuild;
+
+  try {
+    const previousFileSizes = await measureFileSizesBeforeBuild(publicDir);
+
+    fs.emptyDirSync(paths.appBuild);
+
+    copyPublicFolder();
+
+    if (buildNode) {
+      await compile(nodeConfig, 'node');
+      logger.done('build finished');
+      return;
+    }
+
+    const { stats: clientStats } = await compile(clientConfig, 'client');
+
+    if (buildServer) {
+      await compile(serverConfig, 'server');
+    }
+
+    printFileSizesAfterBuild(clientStats, previousFileSizes, publicDir);
+
+    logger.done('build finished');
+  } catch (err) {
+    logger.error('Failed to compile.');
+    logger.error(err.message || err);
+    logger.error(err.stack);
+    process.exit(1);
+  }
+};
