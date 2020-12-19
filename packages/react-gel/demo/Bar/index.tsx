3d0c82752dd2853e7685c3d917e96beb1ba0a1f3
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-12-19 16:41:19 +0000

    remove unused packages

--- packages/react-gel/demo/Bar/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const styles = require('./Bar.module.scss');
-
-export const Bar: React.FC = ({ children }) => <div className={styles.bar}>{children}</div>;
-
-Bar.displayName = 'Bar';
--- packages/react-gel/src/components/grid/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Wrap';
-export * from './Layout';
-export * from './GelItem';
