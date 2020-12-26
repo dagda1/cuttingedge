a0e44df397dd7495a70a959bd0f7ef246ff47214
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2020-12-26 14:09:52 +0000

    update postcss

--- packages/devtools/src/webpack/createPostCssoptions.ts
+++ packages/devtools/src/webpack/createPostCssoptions.ts
@@ -1,29 +1,24 @@
+import postcssNormalize from 'postcss-normalize';
+
 export const createPostCssOptions = (): {
-  postcssOptions: {
-    ident: string;
-    syntax: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    plugins: any[];
-    sourceMap: boolean;
-  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  postcssOptions: { ident: string; syntax: string; plugins: any; sourceMap: boolean };
 } => ({
   postcssOptions: {
     ident: 'postcss',
     syntax: 'postcss-scss',
-    plugins: [
-      'postcss-flexbugs-fixes',
-      [
-        'postcss-preset-env',
-        {
-          autoprefixer: {
-            flexbox: 'no-2009',
-          },
-          stage: 3,
-          features: {
-            'custom-properties': false,
-          },
+    plugins: () => [
+      require('postcss-flexbugs-fixes'),
+      require('postcss-preset-env')({
+        autoprefixer: {
+          flexbox: 'no-2009',
         },
-      ],
+        stage: 3,
+      }),
+      // Adds PostCSS Normalize as the reset css with default options,
+      // so that it honors browserslist config in package.json
+      // which in turn let's users customize the target behavior as per their needs.
+      postcssNormalize(),
     ],
     sourceMap: true,
   },
