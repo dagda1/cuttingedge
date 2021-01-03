c8b4bb4243682c65441c463309c86d37f0776baa
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2021-01-03 15:38:59 +0000

    update useLayoutEffect for now

--- packages/hooks/demo/App.tsx
+++ packages/hooks/demo/App.tsx
@@ -1,23 +1,29 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import type { FC } from 'react';
+import { FC, useEffect, useLayoutEffect } from 'react';
 import { useParentSize } from '../src/useParentSize/useParentSize';
 
 import styles from './global.module.scss';
 
 export const App: FC = () => {
-  const { width, height, ref } = useParentSize(200);
+  const { width, height, ref } = useParentSize(3000);
 
-  // console.log('--------------------');
-  // console.log(width, height, ref);
-  // console.log('--------------------');
+  const el = document.querySelector('.global__child') as HTMLDivElement;
+  useLayoutEffect(() => {
+    if (!el) {
+      return;
+    }
+    console.log('>>>>>>>>', el?.getBoundingClientRect().height);
+  }, [el]);
 
   return (
-    <div ref={ref} className={styles.parent}>
-      <h1>Parent</h1>
-      <div className={styles.child} style={{ width, height }}>
-        <h1>Child</h1>
+    <>
+      <div ref={ref} className={styles.parent}>
+        <h1>Parent</h1>
+        <div className={styles.child} style={{ width, height }}>
+          <h1>Child</h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- packages/hooks/src/useParentSize/useParentSize.ts
+++ packages/hooks/src/useParentSize/useParentSize.ts
@@ -1,13 +1,32 @@
 import { Dimensions, UseParentSizeResult } from './types';
-import { useMemo, useRef, useState } from 'react';
+import { useLayoutEffect, useMemo, useRef, useState } from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
-import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect/useIsomorphicLayoutEffect';
+// import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect/useIsomorphicLayoutEffect';
 import { useDebouncedCallback } from 'use-debounce';
+import { useIsMounted } from '../useIsMounted/useIsMounted';
+
+// const createResizeObserver = () => {
+//   const callbacks: Set<ResizeObserverCallback> = new Set();
+
+//   return {
+//     observer: new ResizeObserver((entries, observer) => {
+//       for (const callback of callbacks) {
+//         callback(entries, observer);
+//       }
+//     }),
+//     subscribe: (callback: ResizeObserverCallback) => callbacks.add(callback),
+//     unsubscribe: (callback: ResizeObserverCallback) => callbacks.delete(callback),
+//   };
+// };
 
 export const useParentSize = (debounceDelay = 0): UseParentSizeResult => {
   const ref = useRef<HTMLElement>(null);
+  const isMounted = useIsMounted();
   const resizeObserverRef = useRef<ResizeObserver | null>();
-  const [dimensions, setDimensions] = useState<Dimensions>({ width: undefined, height: undefined });
+  const [{ width, height }, setDimensions] = useState<Dimensions>({ width: undefined, height: undefined });
+  const previousDimensions = useRef<Dimensions>({ width: undefined, height: undefined });
+  const shortCircuit = useRef(0);
+  shortCircuit.current++;
 
   const { callback: debouncedCallback } = useDebouncedCallback(
     (value: Dimensions) => {
@@ -19,13 +38,12 @@ export const useParentSize = (debounceDelay = 0): UseParentSizeResult => {
     },
   );
 
-  console.log('>>>>>>>>');
-  console.log(debouncedCallback);
-
-  useIsomorphicLayoutEffect(() => {
-    if (!ref.current || !!resizeObserverRef.current || !dimensions) {
+  useLayoutEffect(() => {
+    if (!!resizeObserverRef.current || !ref.current || shortCircuit.current >= 5) {
       return;
     }
+
+    console.log('creating a new one');
 
     resizeObserverRef.current = new ResizeObserver((entries) => {
       if (!Array.isArray(entries) || entries.length !== 1) {
@@ -33,39 +51,42 @@ export const useParentSize = (debounceDelay = 0): UseParentSizeResult => {
       }
 
       const entry = entries[0];
-      const newWidth = Math.round(entry.contentRect.width);
-      const newHeight = Math.round(entry.contentRect.height);
+      const newWidth = Math.round(entry.contentRect.width) - 2;
+      const newHeight = Math.round(entry.contentRect.height) - 2;
 
-      const { width, height } = dimensions;
-
-      console.log({ width, height, newWidth, newHeight });
-
-      if (width !== newWidth || height !== newHeight) {
-        debouncedCallback({ width: newWidth, height: newHeight });
+      console.log({
+        previousHeight: previousDimensions.current?.height,
+        newHeight,
+        elHeight: entry.target.getBoundingClientRect().height,
+      });
+      const newSize = { width: newWidth, height: newHeight };
+      if (previousDimensions.current?.width !== newWidth || previousDimensions.current?.height !== newHeight) {
+        previousDimensions.current.height = newHeight;
+        previousDimensions.current.width = newWidth;
+        if (isMounted.current) {
+          console.log('bouncy bouncy');
+          debouncedCallback(newSize);
+        }
       }
     });
 
     resizeObserverRef.current.observe(ref.current);
 
+    const refElement = ref.current;
     return () => {
-      console.log({ curr: resizeObserverRef.current, currREf: ref.current });
-      console.log(resizeObserverRef?.current?.unobserve);
-      if (!resizeObserverRef.current) {
-        return;
-      }
-
-      if (!!ref.current) {
-        console.log('killing......');
-        resizeObserverRef.current?.unobserve(ref.current);
+      if (!!refElement) {
+        resizeObserverRef.current?.unobserve(refElement);
       }
     };
-  }, []);
+  }, [debouncedCallback, isMounted]);
 
-  console.log('settting the dawg');
-
-  // console.log({ width: dimensions.width, height: dimensions.height });
-
-  const { width, height } = dimensions;
+  if (shortCircuit.current >= 5) {
+    console.log('circuit breaker.......');
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // resizeObserverRef.current?.unobserve(ref.current as any);
+    // resizeObserverRef.current?.disconnect();
+    // resizeObserverRef.current = null;
+  }
 
   return useMemo(
     () => ({
