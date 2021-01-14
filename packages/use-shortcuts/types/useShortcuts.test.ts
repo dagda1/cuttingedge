bce2d7e4cf097ca24c4bf1b36a1b4a8b5696b50f
Paul Cowan <paul.cowan@disclosurescotland.gov.scot>
2021-01-14 15:46:49 +0000

    one dtslint test failing

--- packages/use-shortcuts/types/useShortcuts.test.ts
+++ packages/use-shortcuts/types/useShortcuts.test.ts
@@ -1,44 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-export enum KeyCode {
-  Alt = 'meta',
-  Command = 'command',
-  Ctrl = 'ctrl',
-  DownArrow = 'down',
-  Enter = 'enter',
-  Escape = 'esc',
-  LeftArrow = 'left',
-  Mod = 'mod', // On Mac this ends up mapping to command whereas on Windows and Linux it maps to ctrl.
-  RightArrow = 'right',
-  Shift = 'shift',
-  UpArrow = 'up',
-}
+const f = (i: number): void => console.log(i);
 
-type KeyStroke = KeyCode | string;
+// $ExpectType void
+f(1);
 
-interface Combination {
-  combination: KeyStroke[];
-}
+// Can also write the assertion on the same line.
+f(2); // $ExpectType void
 
-type ShortcutItem<K> = K extends Record<'combination', KeyStroke[]>
-  ? Combination
-  : // : K extends Record<'sequence', KeyStroke[]>
-    // ? Sequence
-    // : K extends ArrayLike<KeyStroke>
-    // ? KeyStroke[]
-    // : K extends string
-    // ? KeyStroke
-    never;
-
-type ShortcutMap<S extends Record<string, unknown>> = {
-  [K in keyof S]: ShortcutItem<S[K]>;
-};
-
-const shortcutMap = {
-  COMBINATION_EXAMPLE: { combination: [KeyCode.Ctrl, 'f'] },
-};
-
-export type CombinationResult = ShortcutMap<typeof shortcutMap>;
-
-declare const o: CombinationResult;
-
-o.COMBINATION_EXAMPLE.combination; // $ExpectType number
+// $ExpectError
+f('one');
