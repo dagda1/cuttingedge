56123b7118f8d91d6b65f04e0a09857ef03c5025
Paul Cowan <paul.cowan@cutting.scot>
2021-01-27 17:18:16 +0000

    remove tailwind

--- packages/devtools/src/webpack/createPostCssoptions.ts
+++ packages/devtools/src/webpack/createPostCssoptions.ts
@@ -1,20 +1,3 @@
-import kebabCase from 'lodash/kebabCase';
-import { paths } from '../config/paths';
-import fs from 'fs';
-import { assert } from 'assert-ts';
-
-assert(fs.existsSync(paths.tailwindcssConfig), `no tailwindcss at ${paths.tailwindcssConfig}`);
-
-export class TailwindExtractor {
-  static extract(content: string): string[] {
-    const matches = content.match(/[A-Za-z0-9-_:/]+/g) || [];
-    const kebab = matches.map((m) => kebabCase(m));
-    return matches.concat(kebab).filter((value, index, self) => {
-      return self.indexOf(value) === index;
-    });
-  }
-}
-
 export const createPostCssOptions = ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   isProduction,
@@ -22,39 +5,17 @@ export const createPostCssOptions = ({
   isProduction: boolean;
 }): {
   sourceMap: boolean;
-  postcssOptions: {
     parser: string;
+    ident: string;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     plugins: any[];
-  };
 } => ({
   sourceMap: true,
-  postcssOptions: {
-    parser: 'postcss-scss',
-    plugins: [
-      'postcss-import',
-      'autoprefixer',
-      require('tailwindcss')(paths.tailwindcssConfig),
-      // isProduction && require('postcss-flexbugs-fixes'),
-      // [
-      //   require('postcss-preset-env'),
-      //   {
-      //     autoprefixer: {
-      //       flexbox: 'no-2009',
-      //     },
-      //     stage: 3,
-      //   },
-      // ],
-      // If you comment out the purgecss section, everything works fine.
-      // require('@fullhuman/postcss-purgecss')({
-      //   content: [`${paths.appSrc}/**/*/tsx`, paths.appHtml],
-      //   extractors: [
-      //     {
-      //       extractor: TailwindExtractor,
-      //       extensions: ['html', 'tsx'],
-      //     },
-      //   ],
-      // }),
-    ].filter(Boolean),
-  },
+  ident: 'postcss',
+  parser: 'postcss-scss',
+  plugins: [
+    require('postcss-import'),
+    require('autoprefixer'),
+    require('postcss-flexbugs-fixes'),
+  ].filter(Boolean),
 });
--- packages/devtools/tools/webpack/createPostCssoptions.d.ts
+++ packages/devtools/tools/webpack/createPostCssoptions.d.ts
@@ -1,6 +1,3 @@
-export declare class TailwindExtractor {
-    static extract(content: string): string[];
-}
 export declare const createPostCssOptions: ({ isProduction, }: {
     isProduction: boolean;
 }) => {
