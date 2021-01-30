46550d29ea2bf96c7ebab623d9b5e8159c2e38b0
Paul Cowan <paul.cowan@cutting.scot>
2021-01-30 14:22:37 +0000

    fix website twiddling

--- packages/website/src/containers/App/index.tsx
+++ packages/website/src/containers/App/index.tsx
@@ -2,7 +2,6 @@ import '@cutting/component-library/dist/component-library.cjs.development.css';
 import type { FC } from 'react';
 import { Routes } from '../../routes';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import '../../assets/scss/global.module.scss';
 
 export const App: FC = () => (
