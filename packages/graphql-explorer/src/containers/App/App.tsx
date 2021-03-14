72b045fd0acbfe7018f800ac6b7bb277e7ccbd5a
Paul Cowan <paul.cowan@cutting.scot>
2021-03-14 09:03:49 +0000

    rebuild

--- packages/graphql-explorer/src/containers/App/App.tsx
+++ packages/graphql-explorer/src/containers/App/App.tsx
@@ -2,6 +2,7 @@ import '@cutting/component-library/dist/component-library.cjs.development.css';
 import type { FC } from 'react';
 import { Explorer } from 'src/containers/Explorer/Explorer';
 import { ApplicationLayout } from '@cutting/component-library';
+import './App.module.scss';
 
 export const App: FC = () => {
   return (
