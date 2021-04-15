f807f9755b42340ab122d35f163c8a76d9e8a4ef
Paul Cowan <paul.cowan@cutting.scot>
2021-04-15 12:03:03 +0100

    use-watcher files

--- packages/the-watcher/src/index.ts
+++ packages/the-watcher/src/index.ts
@@ -1,1 +1,11 @@
-export const watcher = (): void => console.log('watching');
+import program from 'commander';
+
+program
+  .description('Start and watch some scripts based on something')
+  .helpOption('-h, --help', 'show help')
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .action(async (_, options = []) => {
+    await Promise.resolve('herman');
+  });
+
+program.parse(process.argv);
