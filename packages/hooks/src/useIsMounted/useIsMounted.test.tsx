b31f4e62af7dc5b436c62e76119cee220eb1b9e0
Paul Cowan <paul.cowan@cutting.scot>
2021-05-26 09:30:36 +0100

    fix tests

--- packages/hooks/src/useIsMounted/useIsMounted.test.tsx
+++ packages/hooks/src/useIsMounted/useIsMounted.test.tsx
@@ -1,33 +1,15 @@
-import { useEffect } from 'react';
-import { render } from '@testing-library/react';
-import { jest, it, describe, expect } from '@jest/globals';
-
+import { renderHook } from '@testing-library/react-hooks';
+import { it, describe, expect } from '@jest/globals';
 import { useIsMounted } from './useIsMounted';
 
-const TestComponent = ({ fn, delay }: { fn: () => void; delay: number }): JSX.Element => {
-  const isMounted = useIsMounted();
-
-  useEffect(() => {
-    /**
-     * Note that the better way to solve this exact problem is to actually
-     * cancel the timeout in the returning function provided to useEffect.
-     */
-    setTimeout(() => isMounted && fn(), delay);
-  }, [isMounted, delay, fn]);
-
-  return <p />;
-};
-
 describe('useIsMounted', () => {
-  it('should return a boolean indicating if the component is still mounted', () => {
-    jest.useFakeTimers();
-    const mock = jest.fn();
-    const delay = 1000;
-    const { unmount } = render(<TestComponent fn={mock} delay={delay} />);
+  it('returning a ref to the mounted state', () => {
+    const { result, unmount } = renderHook(() => useIsMounted());
+
+    expect(result.current.current).toBe(true);
 
     unmount();
 
-    jest.advanceTimersByTime(delay);
-    expect(mock).not.toHaveBeenCalled();
+    expect(result.current.current).toBe(false);
   });
 });
--- packages/hooks/src/useIsMounted/useIsMounted.ts
+++ packages/hooks/src/useIsMounted/useIsMounted.ts
@@ -1,9 +1,10 @@
 import { useRef, useEffect, MutableRefObject } from 'react';
 
 export const useIsMounted = (): MutableRefObject<boolean> => {
-  const isMountedRef = useRef(true);
+  const isMountedRef = useRef(false);
 
   useEffect(() => {
+    isMountedRef.current = true;
     return () => {
       isMountedRef.current = false;
     };
