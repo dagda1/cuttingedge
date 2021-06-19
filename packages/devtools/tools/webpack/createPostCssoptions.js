da5e11b8ef53bbd35307d0469b3e1995bed8d1b9
Paul Cowan <paul.cowan@cutting.scot>
2021-06-19 11:05:41 +0100

    update postcss

--- packages/devtools/tools/webpack/createPostCssoptions.js
+++ packages/devtools/tools/webpack/createPostCssoptions.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createPostCssOptions = void 0;
+var postcss_normalize_1 = __importDefault(require("postcss-normalize"));
 var createPostCssOptions = function (_a) {
     var 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -11,10 +15,20 @@ var createPostCssOptions = function (_a) {
             ident: 'postcss',
             parser: 'postcss-scss',
             plugins: [
-                require('postcss-import'),
-                require('postcss-url'),
-                require('autoprefixer'),
                 require('postcss-flexbugs-fixes'),
+                [
+                    require('postcss-preset-env'),
+                    {
+                        autoprefixer: {
+                            flexbox: 'no-2009',
+                        },
+                        stage: 3,
+                    },
+                ],
+                // Adds PostCSS Normalize as the reset css with default options,
+                // so that it honors browserslist config in package.json
+                // which in turn let's users customize the target behavior as per their needs.
+                postcss_normalize_1.default(),
             ],
         },
     });
