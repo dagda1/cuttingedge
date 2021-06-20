6f6cd295dae89af33597c5bbb8349e07ecc0f674
Paul Cowan <paul.cowan@cutting.scot>
2021-06-20 07:22:14 +0100

    upgrade postcss

--- packages/devtools/src/webpack/createPostCssoptions.ts
+++ packages/devtools/src/webpack/createPostCssoptions.ts
@@ -1,4 +1,3 @@
-import postcssNormalize from 'postcss-normalize';
 export const createPostCssOptions = ({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   isProduction,
@@ -18,20 +17,10 @@ export const createPostCssOptions = ({
     ident: 'postcss',
     parser: 'postcss-scss',
     plugins: [
+      require('postcss-import'),
+      require('postcss-url'),
+      require('autoprefixer'),
       require('postcss-flexbugs-fixes'),
-      [
-        require('postcss-preset-env'),
-        {
-          autoprefixer: {
-            flexbox: 'no-2009',
-          },
-          stage: 3,
-        },
-      ],
-      // Adds PostCSS Normalize as the reset css with default options,
-      // so that it honors browserslist config in package.json
-      // which in turn let's users customize the target behavior as per their needs.
-      postcssNormalize(),
     ],
   },
 });
