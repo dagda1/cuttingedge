fada7479bf18d25daf7f0de9fe9393b89a798ce5
Paul Cowan <paul.cowan@cutting.scot>
2021-10-23 19:15:20 +0100

    upgade to yarn 3

--- packages/component-library/src/components/atoms/ExternalLink/ExternalLink.css.ts
+++ packages/component-library/src/components/atoms/ExternalLink/ExternalLink.css.ts
@@ -1,8 +1,21 @@
 import { style } from '@vanilla-extract/css';
+import { vars } from 'src';
 import { atoms } from '../../../style/atoms/atoms';
 
 export const root = style([
   atoms({
     reset: 'a',
   }),
+  {
+    color: vars.links.color.link,
+    selectors: {
+      [`&:active`]: {
+        color: vars.links.color.active,
+      },
+      [`&:hover,&:focus`]: {
+        color: vars.links.color.hover,
+        textDecorationThickness: `max(3px, .1875rem, .12em)`,
+      },
+    },
+  },
 ]);
