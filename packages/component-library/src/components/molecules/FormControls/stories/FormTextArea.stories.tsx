1cf9de9e5f9fe64f7b1c2534fd27d7a0d0f7c60f
Paul Cowan <paul.cowan@cutting.scot>
2021-11-05 21:28:16 +0000

    FormTextArea stories

---
+++ packages/component-library/src/components/molecules/FormControls/stories/FormTextArea.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentMeta } from '@storybook/react';
+import { FormTextArea } from '../FormControls';
+import { themedTemplateMaker, themedSelect } from '../../../stories/Stories';
+
+export default {
+  title: 'molecules/FormTextArea',
+  component: FormTextArea,
+  argTypes: {
+    ...themedSelect(),
+    invalid: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    highlight: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    errorMessage: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as ComponentMeta<typeof FormTextArea>;
+
+const Template = themedTemplateMaker(FormTextArea);
+
+export const Simple = Template.bind({});
+
+Simple.args = {
+  label: 'Some label',
+  value: '',
+};
