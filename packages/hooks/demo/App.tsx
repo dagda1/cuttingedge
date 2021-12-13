4a46ce7936f28eba61d214f3aa99eb9a457ea3ab
Paul Cowan <paul.cowan@cutting.scot>
2021-12-13 14:39:43 +0000

    fix hooks tests

--- packages/hooks/demo/App.tsx
+++ packages/hooks/demo/App.tsx
@@ -2,6 +2,7 @@ import '@cutting/component-library/styles.css';
 import { FC, useRef } from 'react';
 import { useScrollToTop } from '../src/useScrollToTop/useScrollToTop';
 import { ApplicationLayout } from '@cutting/component-library';
+import { BrowserRouter } from 'react-router-dom';
 
 import * as styles from './global.css';
 
@@ -10,14 +11,16 @@ export const App: FC = () => {
   useScrollToTop({ ref });
 
   return (
-    <ApplicationLayout heading="@cutting/hooks">
-      <div ref={ref} className={styles.parent}>
-        <h1>Subject</h1>
-      </div>
-      <div className={styles.child}>
-        <h1>Observer</h1>
-      </div>
-    </ApplicationLayout>
+    <BrowserRouter>
+      <ApplicationLayout heading="@cutting/hooks">
+        <div ref={ref} className={styles.parent}>
+          <h1>Subject</h1>
+        </div>
+        <div className={styles.child}>
+          <h1>Observer</h1>
+        </div>
+      </ApplicationLayout>
+    </BrowserRouter>
   );
 };
 
