305312a0789e0cbc58b47c254c995efb89b86e5c
Paul Cowan <paul.cowan@cutting.scot>
2022-03-23 22:10:35 +0000

    refactor omit

--- packages/util/src/object/omit.ts
+++ packages/util/src/object/omit.ts
@@ -1,10 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const omit = <O extends object, K extends (keyof O)[]>(
-  obj: O,
-  ...keys: K[]
-): Pick<O, Exclude<keyof O, K[number]>> =>
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  Object.fromEntries(Object.entries(obj).filter(([k]) => !keys.includes(k as any))) as Pick<
-    O,
-    Exclude<keyof O, K[number]>
-  >;
+export function omit<T, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K> {
+  const copy = {} as T;
+
+  const remaining = (Object.keys(obj) as K[]).flatMap((c) => (keys.includes(c) === false ? [c] : []));
+
+  for (const k of remaining) {
+    copy[k] = obj[k];
+  }
+
+  return copy;
+}
