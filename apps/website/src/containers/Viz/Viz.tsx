86136503a61f1d783e36e5e9633cd23f04bbf1ae
Paul Cowan <paul.cowan@cutting.scot>
2022-04-29 10:53:10 +0100

    add postinstall script

--- apps/website/src/containers/Viz/Viz.tsx
+++ apps/website/src/containers/Viz/Viz.tsx
@@ -1,6 +1,6 @@
-import type { Page } from '../../types';
-import { Route, Routes, Navigate, useLocation } from 'react-router';
-import * as Urls from '../../urls';
+// import type { Page } from '../../types';
+// import { Route, Routes, Navigate, useLocation, Outlet } from 'react-router';
+// import * as Urls from '../../urls';
 import loadable from '@loadable/component';
 import { fallback } from '../../components/Fallback/Fallback';
 import { MathJaxProvider } from '@cutting/use-mathjax';
@@ -9,21 +9,16 @@ const Sine = loadable(() => import('../../components/Sine/Sine'), {
   fallback,
 });
 
-const routes: Page[] = [{ heading: 'Sine wave', path: Urls.Sine, element: <Sine /> }];
+// const routes: Page[] = [{ heading: 'Sine wave', path: Urls.Sine, element: <Sine /> }];
 
-const Default = () => <Navigate to={Urls.Sine} />;
+// const Default = () => <Navigate to={Urls.Sine} />;
 
 export function Viz(): JSX.Element {
-  const { pathname: currentPath } = useLocation();
+  // const { pathname: currentPath } = useLocation();
 
   return (
     <MathJaxProvider>
-      <Routes>
-        {routes.map(({ path, ...rest }) => {
-          return <Route key={path} path={path} {...rest} />;
-        })}
-        <Route path={currentPath} element={<Default />} />
-      </Routes>
+      <Sine />
     </MathJaxProvider>
   );
 }
