a28af86e3aa0cd5f4dd45c27b228a57748fc190d
Paul Cowan <paul.cowan@cutting.scot>
2022-04-29 18:53:18 +0100

    1 passing test in website

--- apps/website/src/components/Footer/Footer.test.tsx
+++ apps/website/src/components/Footer/Footer.test.tsx
@@ -1,11 +1,11 @@
 import { Footer } from '.';
-import { wrapComponentInReduxForTesting } from '../../tests';
+import { wrapComponentForTesting } from '../../tests';
 import { describe, it, expect } from '@jest/globals';
 
 describe('<Footer />', () => {
   it('should render footer', () => {
-    const { container } = wrapComponentInReduxForTesting(<Footer />);
+    const { container } = wrapComponentForTesting(<Footer />);
 
-    expect(container.querySelectorAll('footer a').length).toBeGreaterThan(0);
+    expect(container.querySelectorAll('a').length).toBeGreaterThan(0);
   });
 });
