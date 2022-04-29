66379cc14fbd7224659e6434739fa9e7a456749a
Paul Cowan <paul.cowan@cutting.scot>
2022-04-29 22:19:31 +0100

    fix README

--- packages/devtools/tools/webpack/server.js
+++ packages/devtools/tools/webpack/server.js
@@ -27,6 +27,7 @@ const getExternals = function (isDevelopment) {
                 /\.(svg|png|jpg|jpeg|gif|ico)$/,
                 /\.(mp4|mp3|ogg|swf|webp)$/,
                 /\.(css|scss|sass|sss|less)$/,
+                /\.css.ts$/,
                 /^mathjax-full/,
                 /^@babel/,
                 /^@loadable/,
