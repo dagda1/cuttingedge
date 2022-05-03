a65c64b381729e58d7b13f9adf59c619105706ed
Paul Cowan <paul.cowan@cutting.scot>
2022-05-03 07:26:35 +0100

    reinstate depoy.sh

--- apps/website/bin/deploy.sh
+++ apps/website/bin/deploy.sh
@@ -11,10 +11,10 @@ dcprod build --build-arg CPPFLAGS="-DPNG_ARM_NEON_OPT=0"
 ./bin/stop_production.sh
 ./bin/start_production.sh
 
-# aws ecr get-login-password --region us-east-1  | docker login --username AWS --password-stdin 313095418189.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1  | docker login --username AWS --password-stdin 313095418189.dkr.ecr.us-east-1.amazonaws.com
 
-# docker tag website_nginx:upgrade 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_nginx:upgrade
-# docker tag website_server:upgrade 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_server:upgrade
+docker tag website_nginx:latest 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_nginx:latest
+docker tag website_server:latest 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_server:latest
 
-# docker push 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_server:upgrade
-# docker push 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_nginx:upgrade
+docker push 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_server:latest
+docker push 313095418189.dkr.ecr.us-east-1.amazonaws.com/website_nginx:latest
