ba24b0d629bc19b5058fe684551a79779969f11c
Paul Cowan <paul.cowan@cutting.scot>
2022-09-16 08:16:43 +0100

    revert ansi-regex

--- packages/devtools/tools/jest/setupTests.js
+++ packages/devtools/tools/jest/setupTests.js
@@ -1,6 +1,3 @@
-
 import './polyfills';
 import './raf';
-import { configure } from '@testing-library/dom';
-configure({ testIdAttribute: 'data-selector' });
 //# sourceMappingURL=setupTests.js.map
--- packages/devtools/tools/scripts/test.js
+++ packages/devtools/tools/scripts/test.js
@@ -1,18 +1,16 @@
-
 import { getClientEnv } from '../config/env';
 import fs from 'fs';
 import { paths } from '../config/paths.js';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { run } from 'jest';
-
+// Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 process.on('unhandledRejection', (err) => {
     throw err;
 });
-delete require.cache[require.resolve('../config/env')];
 getClientEnv();
 const argv = process.argv.slice(2);
 argv.push('--no-cache');
