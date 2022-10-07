beb284907c76496abd019392ea6945be2d49c09b
Paul Cowan <paul.cowan@cutting.scot>
2022-10-07 08:34:36 +0100

    update lint-staged

--- packages/use-get-parent-size/demo/src/global.css.ts
+++ packages/use-get-parent-size/demo/src/global.css.ts
@@ -1,18 +1,15 @@
-import type { ComplexStyleRule } from '@vanilla-extract/css';
-import { globalStyle, style } from '@vanilla-extract/css';
+import type { ComplexStyleRule } from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 
 const styles: ComplexStyleRule = {
-  border: '1px solid red',
-  background: 'grey',
-  color: 'white',
-  fontWeight: 'bold',
-  overflow: 'hidden',
-  resize: 'both',
-  width: '200px',
-  height: '200px',
-  // display: 'flex',
-  // justifyContent: 'center',
-  // alignItems: 'center',
+  border: "1px solid red",
+  background: "grey",
+  color: "white",
+  fontWeight: "bold",
+  overflow: "hidden",
+  resize: "both",
+  width: "200px",
+  height: "200px",
 };
 
 export const parent = style({ ...styles });
@@ -20,8 +17,8 @@ export const parent = style({ ...styles });
 export const child = style([
   styles,
   {
-    backgroundColor: 'white',
-    color: 'black',
-    border: '1px solid blue',
+    backgroundColor: "white",
+    color: "black",
+    border: "1px solid blue",
   },
 ]);
