566e2a2976f854a9b3d7bf3d2056e7e6bcc7a673
Paul Cowan <paul.cowan@cutting.scot>
2023-01-06 19:51:24 +0000

    add resolutions to vite.config

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -5,13 +5,19 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 import { isProduction } from '@cutting/util';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), vanillaExtractPlugin(), svgrPlugin({ svgrOptions: { icon: true } }), mdx()],
   assetsInclude: ['src/assets/images/**.png', 'src/assets/images/**.jpg'],
   mode: '',
+  ssr: {
+    format: 'esm',
+  },
   build: {
-    sourcemap: isProduction ? false : 'inline',
+    sourcemap: 'inline',
     minify: isProduction,
     rollupOptions: {
       output: {
@@ -21,4 +27,16 @@ export default defineConfig({
   },
   optimizeDeps: { include: ['react/jsx-runtime'] },
   root: '',
+  resolve: {
+    alias: [
+      {
+        find: '@visx/axis',
+        replacement: require.resolve('@visx/axis/esm/index.js'),
+      },
+      {
+        find: '@visx/shape',
+        replacement: require.resolve('@visx/shape/esm/index.js'),
+      },
+    ],
+  },
 });
