087b6f3f8547a7b94b68378f9f49c985f3575c2d
Paul Cowan <paul.cowan@cutting.scot>
2023-06-06 20:58:33 +0100

    update BLOG

--- apps/website/src/pages/Blog/index.tsx
+++ apps/website/src/pages/Blog/index.tsx
@@ -1,30 +1,36 @@
 import { posts } from './posts';
-import { ExternalLink } from '@cutting/component-library';
+import { ContentBlock, ExternalLink, Inline, Stack, Text } from '@cutting/component-library';
 import { ApplicationLayout } from '~/layouts/ApplicationLayout';
 import { Heading } from '@cutting/component-library';
 
 export function Blog(): JSX.Element {
   return (
     <ApplicationLayout heading="Published blog posts">
-      <p>
-        I blog professionally for&nbsp;
-        <ExternalLink href="https://blog.logrocket.com/author/paulcowan/">Logrocket</ExternalLink>
-        .<br />
-        My personal blog is <ExternalLink href="https://thesoftwaresimpleton.com/">here</ExternalLink>.
-      </p>
-      {posts.map((post, i) => (
-        <div key={i}>
-          <ExternalLink href={post.link} blank={false}>
-            <Heading level="2">{post.title}</Heading>
-          </ExternalLink>
-          <p>{post.summary}</p>
-          <p>
-            <ExternalLink href={post.link} blank={false}>
-              Read More
-            </ExternalLink>
-          </p>
-        </div>
-      ))}
+      <ContentBlock width="large">
+        <Stack space="medium">
+          <Inline space="none">
+            <Text>I blog professionally for&nbsp;</Text>
+            <ExternalLink href="https://blog.logrocket.com/author/paulcowan/">Logrocket</ExternalLink>
+          </Inline>
+          <Inline space="none">
+            <Text>My personal blog is&nbsp;</Text>
+            <ExternalLink href="https://thesoftwaresimpleton.com/">here</ExternalLink>
+          </Inline>
+          {posts.map((post, i) => (
+            <Stack space="medium" key={i}>
+              <ExternalLink href={post.link} blank={false}>
+                <Heading level="2">{post.title}</Heading>
+              </ExternalLink>
+              <Text component="p">{post.summary}</Text>
+              <p>
+                <ExternalLink href={post.link} blank={false}>
+                  Read More
+                </ExternalLink>
+              </p>
+            </Stack>
+          ))}
+        </Stack>
+      </ContentBlock>
     </ApplicationLayout>
   );
 }
