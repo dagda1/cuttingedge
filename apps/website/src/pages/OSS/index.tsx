e2fcb78fe10116d87bc5b1807dc12f66feb77933
Paul Cowan <paul.cowan@cutting.scot>
2023-06-07 21:18:12 +0100

    more OSS

--- apps/website/src/pages/OSS/index.tsx
+++ apps/website/src/pages/OSS/index.tsx
@@ -1,6 +1,5 @@
-import { ContentBlock, ExternalLink, List, Tiles, Stack, Text, Card, Box } from '@cutting/component-library';
+import { ContentBlock, ExternalLink, List, Tiles, Stack, Text, Card, Box, Inline } from '@cutting/component-library';
 import { ApplicationLayout } from '~/layouts/ApplicationLayout';
-import type { Repo } from './repos';
 import { repos } from './repos';
 import { Heading } from '@cutting/component-library';
 import * as styles from './OSS.css';
@@ -12,7 +11,7 @@ export function OSS(): JSX.Element {
       <ContentBlock width="large">
         <Stack space="large">
           <Heading level="2">Merged pull requests to public repositories</Heading>
-          <List type="none" space="small">
+          <Stack space="small">
             <ExternalLink href="https://github.com/backstage/backstage/pulls?q=is%3Apr+is%3Amerged+author%3Adagda1">
               Very active in Backstage
             </ExternalLink>
@@ -31,30 +30,29 @@ export function OSS(): JSX.Element {
             <ExternalLink href="https://github.com/pulls?q=is%3Apr+author%3Adagda1+is%3Apublic+is%3Amerged">
               And many, many, many others....
             </ExternalLink>
-          </List>
+          </Stack>
           <Heading level="2">My Work</Heading>
-          <Tiles space="large" columns={{ mobile: 1, tablet: 2 }}>
-            {repos.map((repo: Repo, i: number) => (
-              <Card height="full" key={i}>
-                <Box width="full" className={styles.repo}>
-                  <ExternalLink href={repo.link}>
-                    <Stack space="large">
-                      <div className={styles.icon}>
-                        <Github />
-                      </div>
-                      <Heading level="2">{repo.name}</Heading>
-                      <Text>{repo.description}</Text>
-                    </Stack>
-                  </ExternalLink>
-                </Box>
-              </Card>
+          <Tiles space="small" columns={{ mobile: 1, tablet: 2, extraWide: 4 }}>
+            {repos.map((repo, i) => (
+              <Box width="full" className={styles.repo} key={i}>
+                <ExternalLink href={repo.link}>
+                  <Stack space="medium">
+                    <div className={styles.icon}>
+                      <Github />
+                    </div>
+                    <Heading level="2">{repo.name}</Heading>
+                    <Text>{repo.description}</Text>
+                  </Stack>
+                </ExternalLink>
+              </Box>
             ))}
           </Tiles>
-          <div>
-            <Heading level="2">
-              For full list of github repos <ExternalLink href="https://github.com/dagda1">click here</ExternalLink>
-            </Heading>
-          </div>
+          <Stack space="small">
+            <Heading level="2">For a full list of github repos</Heading>
+            <ExternalLink href="https://github.com/dagda1">
+              <Text size="large">click here</Text>
+            </ExternalLink>
+          </Stack>
         </Stack>
       </ContentBlock>
     </ApplicationLayout>
