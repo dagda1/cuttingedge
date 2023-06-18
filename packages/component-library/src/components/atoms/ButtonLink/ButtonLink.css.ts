f180a24c7c969a19e1818ed71fcd63893541e89d
Paul Cowan <paul.cowan@cutting.scot>
2023-06-18 10:22:31 +0100

    links

--- packages/component-library/src/components/atoms/ButtonLink/ButtonLink.css.ts
+++ packages/component-library/src/components/atoms/ButtonLink/ButtonLink.css.ts
@@ -1,16 +1,16 @@
 import { globalStyle, style } from '@vanilla-extract/css';
-import { vars } from '~/style/themes/vars.css';
+import * as styles from '../TextLink/TextLink.css';
 
-export const main = style({
-  padding: '14px 22px',
-  display: 'inline-block',
-  textAlign: 'center',
-  ':hover': {
-    borderColor: vars.accessibility.accessibleOutline.backgroundColor,
-    color: vars.links.color.hover,
-    background: vars.accessibility.accessibleOutline.backgroundColor,
-  },
-});
+export const main = style(
+  [
+    {
+      padding: '14px 22px',
+      display: 'inline-block',
+    },
+    styles.base,
+  ],
+  styles.underline,
+);
 
 globalStyle(`${main} span`, {
   padding: 0,
