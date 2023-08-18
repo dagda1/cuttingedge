6739666403922c9503f04cea10194159e6137683
Paul Cowan <paul.cowan@cutting.scot>
2023-08-18 16:11:28 +0100

    return empty array test

--- apps/frontendsupport/app/routes/posts._index.tsx
+++ apps/frontendsupport/app/routes/posts._index.tsx
@@ -1,11 +1,11 @@
 import { useLoaderData } from '@remix-run/react';
-import { getPosts } from '~/utils/post';
+// import { getPosts } from '~/utils/post';
 import { type LoaderFunction } from '@remix-run/node';
 import { Posts } from '~/components/Posts/Posts';
 import type { PostData } from '@cutting/markdown';
 
 export const loader: LoaderFunction = async () => {
-  return getPosts();
+  return [];
 };
 
 export default function PostsHome() {
