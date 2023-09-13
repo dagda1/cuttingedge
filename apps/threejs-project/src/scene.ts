38089881417c8a676be3952c9a8d9130755ff5b3
Paul Cowan <paul.cowan@cutting.scot>
2023-09-13 14:11:57 +0100

    add mozilla security test

--- apps/threejs-project/src/scene.ts
+++ apps/threejs-project/src/scene.ts
@@ -5,91 +5,76 @@ import {
   PerspectiveCamera,
   WebGLRenderer,
   AmbientLight,
-  Color,
   MeshStandardMaterial,
   Mesh,
   SphereGeometry,
-  BoxGeometry,
   PlaneGeometry,
-  TorusGeometry,
   DirectionalLight,
   DirectionalLightHelper,
-  HemisphereLight,
-  HemisphereLightHelper,
-  PointLight,
-  PointLightHelper,
-  RectAreaLight,
-  SpotLight,
-  SpotLightHelper,
-  Vector3,
-  Clock,
+  TextureLoader,
+  LinearSRGBColorSpace,
+  // Clock,
+  PCFSoftShadowMap,
+  CameraHelper,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js';
+import { GUI } from 'lil-gui';
 
 function run() {
   const canvas = document.querySelector('#scene');
 
   assert(!!canvas, `no element at #scene`);
 
+  const gui = new GUI();
   const scene = new Scene();
 
-  const ambientLight = new AmbientLight();
-  ambientLight.color = new Color(0xffffff);
-  ambientLight.intensity = 0.5;
+  const textureLoader = new TextureLoader();
+  const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg');
+
+  const ambientLight = new AmbientLight(0xffffff, 0.5);
+  gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
   scene.add(ambientLight);
 
-  const directionalLight = new DirectionalLight(0x00fffc, 0.3);
-  directionalLight.position.set(1, 0.25, 0);
+  const directionalLight = new DirectionalLight(0xffffff, 0.5);
+
+  directionalLight.castShadow = false;
+  directionalLight.shadow.mapSize.width = 1024;
+  directionalLight.shadow.mapSize.height = 1024;
+
+  directionalLight.position.set(2, 2, -1);
+  gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001);
+  gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
+  gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001);
+  gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001);
+
+  directionalLight.shadow.camera.near = 1;
+  directionalLight.shadow.camera.far = 6;
+
+  directionalLight.shadow.camera.top = 2;
+  directionalLight.shadow.camera.right = 2;
+  directionalLight.shadow.camera.bottom = -2;
+  directionalLight.shadow.camera.left = -2;
+
   scene.add(directionalLight);
 
-  const hemisphereLight = new HemisphereLight(0xff0000, 0x0000ff, 0.3);
-  scene.add(hemisphereLight);
+  const directionalLightHelper = new CameraHelper(directionalLight.shadow.camera);
+  directionalLightHelper.visible = false;
 
-  const directionLightHelper = new DirectionalLightHelper(directionalLight, 0.2);
-  scene.add(directionLightHelper);
-
-  const pointLight = new PointLight(0xff9000, 0.5, 10, 2);
-  pointLight.position.set(1, -0.5, 1);
-  scene.add(pointLight);
-
-  const rectAreaLight = new RectAreaLight(0x4e00ff, 2, 1, 1);
-  rectAreaLight.position.set(-1.5, 0, 1.5);
-  rectAreaLight.lookAt(new Vector3());
-  scene.add(rectAreaLight);
-
-  const spotLight = new SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1);
-  spotLight.position.set(0, 2, 3);
-  scene.add(spotLight);
-
-  const hemisphereLightHelper = new HemisphereLightHelper(hemisphereLight, 0.2);
-  scene.add(hemisphereLightHelper);
-
-  const pointLightHelper = new PointLightHelper(pointLight, 0.2);
-  scene.add(pointLightHelper);
-
-  const rectArealLightHelper = new RectAreaLightHelper(rectAreaLight);
-  scene.add(rectArealLightHelper);
-
-  const spotLightHelper = new SpotLightHelper(spotLight);
-  scene.add(spotLightHelper);
+  scene.add(directionalLightHelper);
 
   const material = new MeshStandardMaterial();
-  material.roughness = 0.4;
+  material.roughness = 0.7;
+  gui.add(material, 'metalness').min(0).max(1).step(0.001);
+  gui.add(material, 'roughness').min(0).max(1).step(0.001);
 
   const sphere = new Mesh(new SphereGeometry(0.5, 32, 32), material);
-  sphere.position.x = -1.5;
-
-  const cube = new Mesh(new BoxGeometry(0.75, 0.75, 0.75), material);
-
-  const torus = new Mesh(new TorusGeometry(0.3, 0.2, 32, 64), material);
-  torus.position.x = 1.5;
+  sphere.castShadow = true;
 
   const plane = new Mesh(new PlaneGeometry(5, 5), material);
   plane.rotation.x = -Math.PI * 0.5;
-  plane.position.y = -0.65;
+  plane.position.y = -0.5;
 
-  scene.add(sphere, cube, torus, plane);
+  scene.add(sphere, plane);
 
   const sizes = {
     width: window.innerWidth,
@@ -117,21 +102,23 @@ function run() {
   controls.enableDamping = true;
 
   const renderer = new WebGLRenderer({ canvas });
+  renderer.outputColorSpace = LinearSRGBColorSpace;
+
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = PCFSoftShadowMap;
+
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-  const clock = new Clock();
+  // const clock = new Clock();
 
   const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-    sphere.rotation.y = 0.1 * elapsedTime;
-    cube.rotation.y = 0.1 * elapsedTime;
-    torus.rotation.y = 0.1 * elapsedTime;
+    // const elapsedTime = clock.getElapsedTime();
 
-    sphere.rotation.x = 0.15 * elapsedTime;
-    cube.rotation.x = 0.15 * elapsedTime;
-    torus.rotation.x = 0.15 * elapsedTime;
-
+    // // Update the sphere
+    // sphere.position.x = Math.cos(elapsedTime) * 1.5;
+    // sphere.position.z = Math.sin(elapsedTime) * 1.5;
+    // sphere.position.y = Math.abs(Math.sin(elapsedTime * 3));
     controls.update();
 
     renderer.render(scene, camera);
