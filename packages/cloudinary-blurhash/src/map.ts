ad925f42cc1290e1a30bd1530e71ba2e42d2d52b
Paul Cowan <paul.cowan@cutting.scot>
2023-09-26 16:32:13 +0100

    cloudinary-blurhash

---
+++ packages/cloudinary-blurhash/src/map.ts
@@ -0,0 +1,70 @@
+import { v2 } from 'cloudinary';
+// import fetch from 'node-fetch';
+// import { promises } from 'fs';
+// import chalk from 'chalk';
+// import logUpdate from 'log-update';
+import { config } from 'dotenv';
+import type { SearchResults, SubFolder } from './types';
+import { getPixels } from '@unpic/pixels';
+import { encode } from 'blurhash';
+
+config();
+
+v2.config({ secure: true });
+
+// v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+console.log(v2.config());
+
+// const jsonFile = './data/map.json';
+
+// // Helper functions
+// const percentage = (partialValue: number, totalValue: number) => ((100 * partialValue) / totalValue).toFixed(2);
+
+// const parseHrtimeToSeconds = (hrtime: [number, number]) => {
+//   const seconds = (hrtime[0] + hrtime[1] / 1e9).toFixed(3);
+//   return seconds;
+// };
+
+// const updateLogs = (a: unknown, b: unknown, c: unknown, d: unknown) =>
+//   logUpdate(
+//     `\n${a} Processed.\n\n${b} images remaining to process\n\n${c} images processed ✅\n\nProcess started ${d}s ago`,
+//   );
+
+const MainFolder = 'frontendsupport';
+
+// Main function
+export async function main(): Promise<void> {
+  // const startTime = process.hrtime();
+  const folder: SubFolder = await v2.api.sub_folders('frontendsupport');
+
+  const allFolders = folder.folders.map((folderItem) => {
+    return folderItem.path;
+  });
+
+  const results: SearchResults<'images'> = await v2.search
+    .expression(`folder:${MainFolder} AND resource_type:image`)
+    .max_results(100)
+    .execute();
+
+  const imageUrls = results.resources.map(({ secure_url, width, height }) => ({ secure_url, width, height }));
+
+  for (const image of imageUrls) {
+    const imgData = await getPixels(image.secure_url);
+
+    const data = Uint8ClampedArray.from(imgData.data);
+
+    const blurhash = encode(data, imgData.width, imgData.height, 4, 4);
+
+    console.log(blurhash);
+  }
+  console.dir({ allFolders, imageUrls }, { depth: 33 });
+}
+
+main().catch((err) => {
+  console.log(err);
+});
---
+++ packages/cloudinary-blurhash/src/types.ts
@@ -0,0 +1,39 @@
+export interface SubFolder {
+  folders: { path: string }[];
+}
+
+export type AssetType = 'image';
+
+type AssetTypes = `${AssetType}s`;
+
+interface SearcResultTotals {
+  total_count: number;
+  time: number;
+}
+
+export interface Resource {
+  asset_id: string;
+  public_id: string;
+  folder: string;
+  filename: string;
+  format: 'jpg' | 'png';
+  version: number;
+  resource_type: 'image' | 'video';
+  type: 'upload';
+  created_at: string;
+  uploaded_at: string;
+  bytes: number;
+  backup_bytes: number;
+  width: number;
+  height: number;
+  aspect_ratio: number;
+  pixels: number;
+  url: string;
+  secure_url: string;
+  status: 'active';
+  access_mode: 'public';
+}
+
+export type SearchResults<A extends AssetTypes> = Record<A, SearcResultTotals> & {
+  resources: Resource[];
+};
