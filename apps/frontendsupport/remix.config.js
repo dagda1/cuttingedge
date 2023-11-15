23e4b51e72ca66c09476e0d0c4d437e3cbe341c5
Paul Cowan <paul.cowan@cutting.scot>
2023-11-15 13:26:16 +0000

    reinstate server

--- apps/frontendsupport/remix.config.js
+++ apps/frontendsupport/remix.config.js
@@ -2,15 +2,13 @@ import { flatRoutes } from 'remix-flat-routes';
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ['**/*'],
+  ignoredRouteFiles: ['**/.*', '**/*.css.ts'],
   serverModuleFormat: 'esm',
   serverPlatform: 'node',
-  serverBuildPath: 'server/index.mjs',
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  routes: async (defineRoutes) => {
-    return flatRoutes('routes', defineRoutes, {
-      ignoredRouteFiles: ['.*', '**/*.css', '**/*.css.ts', '**/*.test.{js,jsx,ts,tsx}', '**/__*.*'],
-    });
-  },
+  serverBuildPath: 'build/index.mjs',
+  // routes: async (defineRoutes) => {
+  //   return flatRoutes('routes', defineRoutes, {
+  //     ignoredRouteFiles: ['.*', '**/*.css', '**/*.css.ts', '**/*.test.{js,jsx,ts,tsx}', '**/__*.*'],
+  //   });
+  // },
 };
