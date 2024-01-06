4678a85b66d9baeb877bb5e59d4c91101e3e63c0
Paul Cowan <paul.cowan@cutting.scot>
2024-01-06 12:57:17 +0000

    remove unused panels

--- apps/frontendsupport/app/pages/Panels/Help/HelpPanel.css.ts
+++
@@ -1,9 +0,0 @@
-import { vars } from '@cutting/component-library';
-import { style } from '@vanilla-extract/css';
-
-export const heading = style({
-  position: 'relative',
-  zIndex: 3,
-  top: '-30%',
-  background: vars.backgroundColor.body,
-});
--- apps/frontendsupport/app/pages/Panels/Highlights/Highlights.css.ts
+++
@@ -1,8 +0,0 @@
-import { palette } from '@cutting/component-library';
-import { globalStyle, style } from '@vanilla-extract/css';
-
-export const container = style({});
-
-globalStyle(`${container} a`, {
-  color: palette.white,
-});
