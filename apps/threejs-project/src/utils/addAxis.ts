5521b50aa2f64b514968e4881d504256bb4cdac4
Paul Cowan <paul.cowan@cutting.scot>
2024-03-24 14:19:01 +0000

    separate util methods

--- apps/threejs-project/src/utils/addAxis.ts
+++ apps/threejs-project/src/utils/addAxis.ts
@@ -1,19 +1,56 @@
-import type { ColorRepresentation, Scene } from 'three';
-import { CylinderGeometry, Mesh, MeshBasicMaterial, Vector3 } from 'three';
+import type { ScaleLinear } from 'd3-scale';
+import type { Material, Scene } from 'three';
+import { BufferGeometry, Line, Mesh, MeshBasicMaterial, Vector2 } from 'three';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 
-export function addAxis(scene: Scene, start: Vector3, end: Vector3, color: ColorRepresentation = 0x000000): void {
-  const direction = new Vector3().subVectors(end, start);
-  const length = direction.length();
-  const cylinderGeometry = new CylinderGeometry(0.02, 0.02, length, 8);
-  const cylinderMaterial = new MeshBasicMaterial({ color });
-  const cylinder = new Mesh(cylinderGeometry, cylinderMaterial);
+export function createLine(start: Vector2, end: Vector2, material: Material): Line {
+  const geometry = new BufferGeometry().setFromPoints([start, end]);
 
-  cylinder.position.x = (start.x + end.x) / 2;
-  cylinder.position.y = (start.y + end.y) / 2;
-  cylinder.position.z = (start.z + end.z) / 2;
+  return new Line(geometry, material);
+}
 
-  cylinder.lookAt(end);
-  cylinder.rotateX(Math.PI / 2); // Align with the direction vector
+export function createTickMarks(
+  scene: Scene,
+  xScale: ScaleLinear<number, number, never>,
+  yScale: ScaleLinear<number, number, never>,
+  scale: ScaleLinear<number, number, never>,
+  orientation: 'x' | 'y',
+  tickMaterial: Material,
+  tickSize = 8,
+): void {
+  const fontLoader = new FontLoader();
+  const textMaterial = new MeshBasicMaterial({ color: 0xffffff });
 
-  scene.add(cylinder);
+  // Load a font and create the tick marks and labels
+  fontLoader.load('/fonts/helvetiker_regular.typeface.json', function (font) {
+    for (const tick of scale.ticks()) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      let start, end, labelPos: any;
+      const tickValue = tick.toString();
+      if (orientation === 'x') {
+        start = new Vector2(xScale(tick), yScale(0) - tickSize);
+        end = new Vector2(xScale(tick), yScale(0) + tickSize);
+        labelPos = new Vector2(xScale(tick) - 3, yScale(0) + tickSize - 30);
+      } else if (orientation === 'y') {
+        start = new Vector2(xScale(0) - tickSize, yScale(tick));
+        end = new Vector2(xScale(0) + tickSize, yScale(tick));
+        labelPos = new Vector2(xScale(0) + tickSize - 30, yScale(tick) - 4);
+      }
+      const tickMark = createLine(start as Vector2, end as Vector2, tickMaterial);
+      scene.add(tickMark);
+
+      // Create text label
+      const textGeometry = new TextGeometry(tickValue, {
+        font: font,
+        size: 10,
+        height: 1,
+        curveSegments: 12,
+        bevelEnabled: false,
+      });
+      const textMesh = new Mesh(textGeometry, textMaterial);
+      textMesh.position.set(labelPos.x, labelPos.y, 0);
+      scene.add(textMesh);
+    }
+  });
 }
