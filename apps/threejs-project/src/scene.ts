f5aff56807da44dfd510cd21bc166a9b9e2f220c
Paul Cowan <paul.cowan@cutting.scot>
2024-03-24 15:08:12 +0000

    axes labels and arrows

--- apps/threejs-project/src/scene.ts
+++ apps/threejs-project/src/scene.ts
@@ -11,11 +11,14 @@ import {
   Vector3,
   EdgesGeometry,
   LineSegments,
+  ArrowHelper,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-// import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { addTick } from './utils/addTick';
 import { addAxis } from './utils/addAxis';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { axes } from './types';
 
 function run() {
   const canvas = document.querySelector<HTMLElement>('#scene');
@@ -35,6 +38,7 @@ function run() {
   scene.add(edgesMesh);
 
   const axisLength = 3;
+
   addAxis(scene, new Vector3(-axisLength, 0, 0), new Vector3(axisLength, 0, 0));
   addAxis(scene, new Vector3(0, -axisLength, 0), new Vector3(0, axisLength, 0));
   addAxis(scene, new Vector3(0, 0, -axisLength), new Vector3(0, 0, axisLength));
@@ -45,6 +49,94 @@ function run() {
     addTick(scene, position, 'x');
     addTick(scene, position, 'y');
     addTick(scene, position, 'z');
+  });
+
+  const arrowSize = 0.5;
+  const axesInfo = [
+    { dir: new Vector3(1, 0, 0), color: 0xff0000, label: 'X' },
+    { dir: new Vector3(0, 0, 1), color: 0x0000ff, label: 'Y' },
+    { dir: new Vector3(0, 1, 0), color: 0x00ff00, label: 'Z' },
+  ];
+
+  for (const axis of axesInfo) {
+    const arrowHelper = new ArrowHelper(axis.dir, new Vector3(0, 0, 0), axisLength + 0.5, axis.color, arrowSize);
+    scene.add(arrowHelper);
+  }
+
+  const loader = new FontLoader();
+
+  loader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+    for (const axis of axesInfo) {
+      const textGeometry = new TextGeometry(axis.label, {
+        font: font,
+        size: 0.1,
+        height: 0.1,
+      });
+
+      const textMaterial = new MeshBasicMaterial({ color: axis.color });
+      const textMesh = new Mesh(textGeometry, textMaterial);
+
+      // Position the text mesh at the end of the arrow
+      const offset = axisLength + 0.5; // Adjust based on your preference
+      textMesh.position.copy(axis.dir.clone().multiplyScalar(offset));
+
+      scene.add(textMesh);
+    }
+
+    const tickPositions = [-3, -2, -1, 0, 1, 2, 3];
+    const fontSize = 0.1;
+    const fontHeight = 0.01;
+    const fontColor = 0x000000;
+
+    for (const axis of axes) {
+      for (const pos of tickPositions) {
+        const textGeometry = new TextGeometry(Number(pos).toString(), {
+          font: font,
+          size: fontSize,
+          height: fontHeight,
+        });
+
+        const textMaterial = new MeshBasicMaterial({ color: fontColor });
+        const textMesh = new Mesh(textGeometry, textMaterial);
+
+        switch (axis) {
+          case 'x':
+            textMesh.position.x = pos - 0.1;
+            textMesh.position.y = -0.3;
+            textMesh.position.z = 0;
+            break;
+          case 'y':
+            textMesh.position.x = 0.1;
+            textMesh.position.y = pos;
+            textMesh.position.z = 0;
+            break;
+          case 'z':
+            textMesh.position.x = 0.1;
+            textMesh.position.y = 0;
+            textMesh.position.z = pos;
+            break;
+        }
+
+        scene.add(textMesh);
+      }
+    }
+
+    // for (const pos of tickPositions) {
+    //   const textGeometry = new TextGeometry(Number(pos).toString(), {
+    //     font: font,
+    //     size: fontSize,
+    //     height: fontHeight,
+    //   });
+
+    //   const textMaterial = new MeshBasicMaterial({ color: fontColor });
+    //   const textMesh = new Mesh(textGeometry, textMaterial);
+
+    //   textMesh.position.x = 0.2;
+    //   textMesh.position.y = pos - 0.1;
+    //   textMesh.position.z = 0;
+
+    //   scene.add(textMesh);
+    // }
   });
 
   // const loader = new FontLoader();
@@ -70,8 +162,6 @@ function run() {
   //     textMesh.position.z = pos - 0.5;
 
   //     scene.add(textMesh);
-  //   });
-  // });
 
   const sizes = {
     width: window.innerWidth,
