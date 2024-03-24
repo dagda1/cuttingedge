01ed45ee0d61f7e8d3cec4233879fffb78e27ef9
Paul Cowan <paul.cowan@cutting.scot>
2024-03-24 14:19:01 +0000

    separate util methods

--- apps/threejs-project/src/utils/addAxis.ts
+++ apps/threejs-project/src/utils/addAxis.ts
@@ -1,19 +1,12 @@
-import type { ColorRepresentation } from 'three';
-import { type Scene, Vector3, CylinderGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { BufferGeometry, Line, LineBasicMaterial, type Scene, type Vector3 } from 'three';
 
-export function addAxis(scene: Scene, start: Vector3, end: Vector3, color: ColorRepresentation = 0x000000): void {
-  const direction = new Vector3().subVectors(end, start);
-  const length = direction.length();
-  const cylinderGeometry = new CylinderGeometry(0.02, 0.02, length, 8);
-  const cylinderMaterial = new MeshBasicMaterial({ color });
-  const cylinder = new Mesh(cylinderGeometry, cylinderMaterial);
+export function addAxis(scene: Scene, start: Vector3, end: Vector3): void {
+  const material = new LineBasicMaterial({ color: 0x000000 });
+  const points = [];
+  points.push(start.clone());
+  points.push(end.clone());
 
-  cylinder.position.x = (start.x + end.x) / 2;
-  cylinder.position.y = (start.y + end.y) / 2;
-  cylinder.position.z = (start.z + end.z) / 2;
-
-  cylinder.lookAt(end);
-  cylinder.rotateX(Math.PI / 2); // Align with the direction vector
-
-  scene.add(cylinder);
+  const geometry = new BufferGeometry().setFromPoints(points);
+  const line = new Line(geometry, material);
+  scene.add(line);
 }
