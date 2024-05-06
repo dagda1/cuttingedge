da0a0db1d8b9802b888c5daa16088d7ca5c4594e
Paul Cowan <paul.cowan@cutting.scot>
2024-05-06 19:36:40 +0100

    fix TopNav and contact page

--- apps/frontendsupport/app/components/Header/Header.tsx
+++ apps/frontendsupport/app/components/Header/Header.tsx
@@ -3,15 +3,13 @@ import { Box } from '@cutting/component-library';
 
 export function Header(): JSX.Element {
   return (
-    <Box component="header" position="fixed" width="full" zIndex="modal" top={0} style={{ background: 'inherit' }}>
+    <Box component="header" position="fixed" width="full" zIndex="modal" style={{ background: 'inherit' }}>
       <Box
         height="xxxlarge"
-        paddingX={{ mobile: 'medium', tablet: 'medium' }}
         display="flex"
         alignItems="center"
         justifyContent="spaceAround"
         paddingY="small"
-        overflowX="hidden"
         width="full"
       >
         <TopNav />
