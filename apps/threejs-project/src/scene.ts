e2c1114f239e7f931d6769e566fe3126a0bfed89
Paul Cowan <paul.cowan@cutting.scot>
2024-05-22 21:10:26 +0100

    add ticks and labels to dotproduct grid

--- apps/threejs-project/src/scene.ts
+++ apps/threejs-project/src/scene.ts
@@ -1,159 +1,55 @@
 import './style.css';
 import { assert } from '@cutting/assert';
+import { Vector2 } from 'three';
 import {
   Scene,
   WebGLRenderer,
-  PerspectiveCamera,
-  MeshBasicMaterial,
-  Mesh,
+  LinearSRGBColorSpace,
+  PCFSoftShadowMap,
   LineBasicMaterial,
-  Vector3,
-  ArrowHelper,
-  BufferGeometry,
-  Line,
+  OrthographicCamera,
 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { addTick } from './utils/addTick';
-import { addAxis } from './utils/addAxis';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
-import { axes } from './types';
+import { scaleLinear } from 'd3-scale';
+import { createLine, createTickMarks } from './utils/axis';
+// import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+// import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+// import { axes } from './types';
 
-const AxisLength = 3;
+const RANGE = [-10, 10];
 
 function run() {
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  const xScale = scaleLinear().domain(RANGE).range([0, sizes.width]);
+  const yScale = scaleLinear().domain(RANGE).range([sizes.height, 0]);
+
   const canvas = document.querySelector<HTMLElement>('#scene');
 
   assert(!!canvas, `no element at #scene`);
 
   const scene = new Scene();
 
-  addAxis(scene, new Vector3(-AxisLength, 0, 0), new Vector3(AxisLength, 0, 0));
-  addAxis(scene, new Vector3(0, -AxisLength, 0), new Vector3(0, AxisLength, 0));
-  addAxis(scene, new Vector3(0, 0, -AxisLength), new Vector3(0, 0, AxisLength));
+  const lineMaterial = new LineBasicMaterial({ color: 0xffffff });
+  const tickMaterial = new LineBasicMaterial({ color: 0xffffff });
 
-  const tickPositions = [-3, -2, -1, 0, 1, 2, 3];
+  const xAxisStart = new Vector2(xScale(-10), yScale(0));
+  const xAxisEnd = new Vector2(xScale(10), yScale(0));
 
-  tickPositions.forEach((position) => {
-    addTick(scene, position, 'x');
-    addTick(scene, position, 'y');
-    addTick(scene, position, 'z');
-  });
+  const xAxis = createLine(xAxisStart, xAxisEnd, lineMaterial);
 
-  const arrowSize = 0.1;
-  const axesInfo = [
-    { dir: new Vector3(1, 0, 0), color: 0x000000, label: 'X' },
-    { dir: new Vector3(0, 0, 1), color: 0x000000, label: 'Y' },
-    { dir: new Vector3(0, 1, 0), color: 0x000000, label: 'Z' },
-  ];
+  scene.add(xAxis);
 
-  for (const axis of axesInfo) {
-    const arrowHelper = new ArrowHelper(axis.dir, new Vector3(0, 0, 0), AxisLength + 0.2, axis.color, arrowSize, 0.1);
-    scene.add(arrowHelper);
-  }
+  const yAxisStart = new Vector2(xScale(0), yScale(-10));
+  const yAxisEnd = new Vector2(xScale(0), yScale(10));
+  const yAxis = createLine(yAxisStart, yAxisEnd, lineMaterial);
 
-  const loader = new FontLoader();
+  scene.add(yAxis);
 
-  loader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
-    for (const axis of axesInfo) {
-      const textGeometry = new TextGeometry(axis.label, {
-        font: font,
-        size: 0.1,
-        height: 0.1,
-      });
-
-      const textMaterial = new MeshBasicMaterial({ color: axis.color });
-      const textMesh = new Mesh(textGeometry, textMaterial);
-
-      const offset = AxisLength + 0.3;
-      textMesh.position.copy(axis.dir.clone().multiplyScalar(offset));
-
-      scene.add(textMesh);
-    }
-
-    const tickPositions = [-3, -2, -1, 0, 1, 2, 3];
-    const fontSize = 0.1;
-    const fontHeight = 0.01;
-    const fontColor = 0x000000;
-
-    for (const axis of axes) {
-      for (const pos of tickPositions) {
-        const textGeometry = new TextGeometry(Number(pos).toString(), {
-          font: font,
-          size: fontSize,
-          height: fontHeight,
-        });
-
-        const textMaterial = new MeshBasicMaterial({ color: fontColor });
-        const textMesh = new Mesh(textGeometry, textMaterial);
-
-        switch (axis) {
-          case 'x':
-            textMesh.position.x = pos - 0.1;
-            textMesh.position.y = -0.3;
-            textMesh.position.z = 0;
-            break;
-          case 'y':
-            textMesh.position.x = 0.1;
-            textMesh.position.y = pos;
-            textMesh.position.z = 0;
-            break;
-          case 'z':
-            textMesh.position.x = 0.1;
-            textMesh.position.y = 0;
-            textMesh.position.z = pos;
-            break;
-        }
-
-        scene.add(textMesh);
-      }
-    }
-  });
-
-  const GridPoints = [-3, -2.5, -2, -1.5, -1, -0.5, 0.5, 1, 1.5, 2, 2.5, 3] as const;
-
-  for (const x of GridPoints) {
-    const start = new Vector3(x, 0, -AxisLength);
-
-    const end = new Vector3(x, 0, AxisLength);
-
-    const material = new LineBasicMaterial({ color: 0x000000 });
-
-    const points = [];
-
-    points.push(start.clone());
-    points.push(end.clone());
-
-    const geometry = new BufferGeometry().setFromPoints(points);
-    const line = new Line(geometry, material);
-    scene.add(line);
-  }
-
-  for (const x of GridPoints) {
-    const start = new Vector3(-AxisLength, 0, x);
-
-    const end = new Vector3(AxisLength, 0, x);
-
-    const material = new LineBasicMaterial({ color: 0x000000 });
-
-    const points = [];
-
-    points.push(start.clone());
-    points.push(end.clone());
-
-    const geometry = new BufferGeometry().setFromPoints(points);
-    const line = new Line(geometry, material);
-    scene.add(line);
-  }
-
-  addAxis(scene, new Vector3(0, 0, 0), new Vector3(2, 2, 2), 0xff0000);
-
-  addAxis(scene, new Vector3(0, 0, 0), new Vector3(1, 0, 1), 0x0000ff);
-
-  const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  };
+  createTickMarks(scene, xScale, yScale, xScale, 'x', tickMaterial);
+  createTickMarks(scene, xScale, yScale, yScale, 'y', tickMaterial);
 
   window.addEventListener('resize', () => {
     sizes.width = window.innerWidth;
@@ -165,32 +61,24 @@ function run() {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   });
 
-  const camera = new OrthographicCamera(
-    window.innerWidth / -2, // Left
-    window.innerWidth / 2, // Right
-    window.innerHeight / 2, // Top
-    window.innerHeight / -2, // Bottom
-    1, // Near clipping plane
-    1000, // Far clipping plane
-  );
-
-  // Position the camera
-  camera.position.set(0, 0, 100);
-  camera.lookAt(0, 0, 0);
+  const camera = new OrthographicCamera(0, sizes.width, sizes.height, 0, -1, 1);
+  camera.position.z = 1;
 
   const renderer = new WebGLRenderer({
     canvas: canvas,
   });
-
-  renderer.setClearColor(0xffffff, 1);
+  renderer.outputColorSpace = LinearSRGBColorSpace;
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = PCFSoftShadowMap;
+  renderer.setClearColor('#262837');
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
   const tick = () => {
-    controls.update();
-
+    // Render
     renderer.render(scene, camera);
 
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick);
   };
   tick();
