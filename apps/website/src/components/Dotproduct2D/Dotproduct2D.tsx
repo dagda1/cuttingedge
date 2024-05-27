90c4aebdeb20226efb9df58137b852740976c7df
Paul Cowan <paul.cowan@cutting.scot>
2024-05-27 16:13:11 +0100

    add projection functions to scene

--- apps/website/src/components/Dotproduct2D/Dotproduct2D.tsx
+++ apps/website/src/components/Dotproduct2D/Dotproduct2D.tsx
@@ -3,6 +3,7 @@ import { Box } from '@cutting/component-library';
 import { useParentSize } from '@cutting/use-get-parent-size';
 import { useRef } from 'react';
 import { ApplicationLayout } from '~/layouts/ApplicationLayout';
+import type { Material } from 'three';
 import { Vector2 } from 'three';
 import {
   Scene,
@@ -12,11 +13,71 @@ import {
   LineBasicMaterial,
   OrthographicCamera,
 } from 'three';
+import type { ScaleLinear } from 'd3-scale';
 import { scaleLinear } from 'd3-scale';
 import { useIsomorphicLayoutEffect } from '@cutting/hooks';
 import { createLine, createTickMarks } from './axes';
 
 const RANGE = [-10, 10];
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+interface Line {
+  start: Point;
+  end: Point;
+}
+
+type TwoDVector = [number, number];
+
+const LineA: Line = { start: { x: 1, y: 2 }, end: { x: 6, y: 8 } };
+
+const LineB: Line = { start: { x: 1, y: 2 }, end: { x: 7, y: -4 } };
+
+function directionVector(line: Line): TwoDVector {
+  return [line.end.x - line.start.x, line.end.y - line.start.y];
+}
+
+function AddLineToGraph(
+  xScale: ScaleLinear<number, number, never>,
+  yScale: ScaleLinear<number, number, never>,
+  line: Line,
+  material: Material,
+) {
+  return createLine(
+    new Vector2(xScale(line.start.x), yScale(line.start.y)),
+    new Vector2(xScale(line.end.x), yScale(line.end.y)),
+    material,
+  );
+}
+
+function projectLineAOntoLineB(
+  xScale: ScaleLinear<number, number, never>,
+  yScale: ScaleLinear<number, number, never>,
+  lineA: Line,
+  lineB: Line,
+  material: Material,
+) {
+  const directionVectorA = directionVector(lineA);
+  const directionVectorB = directionVector(lineB);
+
+  const dotProductAB = directionVectorA[0] * directionVectorB[0] + directionVectorA[1] * directionVectorB[1];
+  const bSquared = directionVectorB[0] ** 2 + directionVectorB[1] ** 2;
+  const p = dotProductAB / bSquared;
+
+  const projbA = [directionVectorB[0] * p, directionVectorB[1] * p];
+
+  const projectionLine: Line = {
+    start: { ...LineA.start },
+    end: { x: lineA.start.x + projbA[0], y: lineA.start.y + projbA[1] },
+  };
+
+  console.log({ projbA, projectionLine });
+
+  AddLineToGraph(xScale, yScale, projectionLine, material);
+}
 
 export function DotProduct2D(): JSX.Element {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -31,38 +92,46 @@ export function DotProduct2D(): JSX.Element {
       return;
     }
 
-    const xScale = scaleLinear().domain(RANGE).range([0, width]);
-    const yScale = scaleLinear().domain(RANGE).range([height, 0]);
+    const minUnit = Math.min(width, height);
+    const xScale = scaleLinear().domain(RANGE).range([0, minUnit]);
+    const yScale = scaleLinear().domain(RANGE).range([0, minUnit]);
 
     const scene = new Scene();
 
     const lineMaterial = new LineBasicMaterial({ color: 0xffffff });
     const tickMaterial = new LineBasicMaterial({ color: 0xffffff });
+    const vectorMaterial = new LineBasicMaterial({ color: 0xff0000 });
+    const projectionMaterial = new LineBasicMaterial({ color: 0x0000ff });
 
-    const xAxisStart = new Vector2(xScale(-10), yScale(0));
-    const xAxisEnd = new Vector2(xScale(10), yScale(0));
-
-    const xAxis = createLine(xAxisStart, xAxisEnd, lineMaterial);
+    const xAxis = createLine(new Vector2(xScale(-10), yScale(0)), new Vector2(xScale(10), yScale(0)), lineMaterial);
 
     scene.add(xAxis);
 
-    const yAxisStart = new Vector2(xScale(0), yScale(-10));
-    const yAxisEnd = new Vector2(xScale(0), yScale(10));
-    const yAxis = createLine(yAxisStart, yAxisEnd, lineMaterial);
+    const yAxis = createLine(new Vector2(xScale(0), yScale(-10)), new Vector2(xScale(0), yScale(10)), lineMaterial);
 
     scene.add(yAxis);
 
     createTickMarks(scene, xScale, yScale, xScale, 'x', tickMaterial);
     createTickMarks(scene, xScale, yScale, yScale, 'y', tickMaterial);
 
+    const lineA = AddLineToGraph(xScale, yScale, LineA, vectorMaterial);
+
+    scene.add(lineA);
+
+    const lineB = AddLineToGraph(xScale, yScale, LineB, vectorMaterial);
+
+    scene.add(lineB);
+
+    projectLineAOntoLineB(xScale, yScale, LineA, LineB, projectionMaterial);
+
     window.addEventListener('resize', () => {
       camera.updateProjectionMatrix();
 
-      renderer.setSize(width, height);
+      renderer.setSize(minUnit, minUnit);
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
     });
 
-    const camera = new OrthographicCamera(0, width, height, 0, -1, 1);
+    const camera = new OrthographicCamera(0, minUnit, minUnit, 0, -1, 1);
     camera.position.z = 1;
 
     const renderer = new WebGLRenderer({
@@ -72,11 +141,10 @@ export function DotProduct2D(): JSX.Element {
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = PCFSoftShadowMap;
     renderer.setClearColor('#262837');
-    renderer.setSize(width, height);
+    renderer.setSize(minUnit, minUnit);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
     const tick = () => {
-      // Render
       renderer.render(scene, camera);
 
       // Call tick again on the next frame
@@ -87,7 +155,7 @@ export function DotProduct2D(): JSX.Element {
 
   return (
     <ApplicationLayout centerHeading heading="DOT PRODUCT 2D" showFooter={false}>
-      <Box component="section" ref={containerRef}>
+      <Box component="section" ref={containerRef} display="flex" justifyContent="center">
         <canvas id="scene" ref={canvasRef}></canvas>
       </Box>
     </ApplicationLayout>
