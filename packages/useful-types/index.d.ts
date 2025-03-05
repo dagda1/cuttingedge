fac6b4539a4c5699de21c6b0434fd553ad66d418
Paul <paul.cowan@cutting.scot>
2025-03-05 08:48:35 +0000

    Fix useful types (#1029)
    * split globabl.d.ts
    
    * introduce new jsx.d.ts to @cutting/useful-types

---
+++ packages/useful-types/index.d.ts
@@ -0,0 +1,34 @@
+declare let __BROWSER__: boolean;
+declare let __DEV__: boolean;
+
+declare module '*.png' {
+  const pngValue: any;
+  export default pngValue;
+}
+
+declare module '*.jpg' {
+  const jpgValue: any;
+  export default value;
+}
+
+interface ViteHotContext {
+  readonly data: any;
+
+  accept(): void;
+  accept(cb: (mod: any) => void): void;
+  accept(dep: string, cb: (mod: any) => void): void;
+  accept(deps: readonly string[], cb: (mods: any[]) => void): void;
+
+  dispose(cb: (data: any) => void): void;
+  decline(): void;
+  invalidate(): void;
+ 
+  on<T extends string>(event: T, cb: (payload: any) => void): void;
+  send<T extends string>(event: T, data?: any): void;
+}
+
+interface ImportMeta {
+  url: string;
+
+  readonly hot?: ViteHotContext;
+}
