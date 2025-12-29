2d30e04ecb6e776cea88345184c751524d1adf45
Paul Cowan <paul.cowan@cutting.scot>
2025-12-29 16:46:53 +0000

    add deploy-frontendsupport github action

--- scripts/create-npmrc.sh
+++ scripts/create-npmrc.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 NPM_TOKEN=$1
-GSAP_TOKEN=$2
 
 cat << EOF > "$HOME/.npmrc"
 always-auth=true
 email=paul.cowan@cutting.scot
 //registry.npmjs.org/:_authToken=$NPM_TOKEN
-//npm.greensock.com/:_authToken=$GSAP_TOKEN
 @gsap:registry=https://npm.greensock.com
 EOF
